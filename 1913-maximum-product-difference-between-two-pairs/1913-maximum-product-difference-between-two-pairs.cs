10930bef8ba5932d5e006607523ceb82a1b1b6b6
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-05-22 13:09:24 +0300

    Time: 113 ms (65.56%), Space: 45.9 MB (26.67%) - LeetHub

---
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cs
@@ -0,0 +1,7 @@
+public class Solution {
+    public int MaxProductDifference(int[] nums) {
+        Array.Sort(nums);
+        
+        return (nums[^1] * nums[^2]) - (nums[0] * nums[1]);
+    }
+}
