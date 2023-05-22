be88ec6d2a90b632ad979769842e17dd0edef8b8
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-05-22 13:05:43 +0300

    Time: 95 ms (64.82%), Space: 39.5 MB (90.74%) - LeetHub

---
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int CountKDifference(int[] nums, int k) {
+        var result = 0;
+        var length = nums.Length;
+        
+        for(var i = 0; i<length; i++){
+            for(var j = i + 1; j<length; j++){
+                if(Math.Abs(nums[i] - nums[j]) ==k && i< j)   result++; 
+            
+            }
+        }
+        
+        return result;
+    }
+}
