4d699d364a458f3fc8e314f12ae828b2e36fc305
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-05-22 14:51:18 +0300

    Time: 82 ms (84.95%), Space: 39.4 MB (12.90%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cs
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cs
@@ -1,8 +1,8 @@
 public class Solution {
     public int MaxProduct(int[] nums) {
-      Array.Sort(nums);
-        
-      return (nums[^1] - 1) * (nums[^2]  - 1);
+        Array.Sort(nums); // O(log n) 
+        return (nums[^1] - 1) * (nums[^2] - 1);
     }
 }
 
+// [2,3,4,5]  (4-1) * (5-1) = 12
