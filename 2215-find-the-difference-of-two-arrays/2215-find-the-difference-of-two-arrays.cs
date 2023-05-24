5c1145064f14771f65f205652c4d66045500a7ce
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-05-24 12:34:17 +0300

    Time: 166 ms (93.46%), Space: 58.8 MB (40.50%) - LeetHub

---
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cs
@@ -0,0 +1,8 @@
+public class Solution {
+    public IList<IList<int>> FindDifference(int[] nums1, int[] nums2) {
+       return new[]{
+           nums1.Except(nums2).ToArray(),
+           nums2.Except(nums1).ToArray()
+       };
+    }
+}
