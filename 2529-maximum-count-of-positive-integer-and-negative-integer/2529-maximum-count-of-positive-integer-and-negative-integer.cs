42e33aa8662e7140748788719594c4bcd61c3905
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-05-25 10:44:02 +0300

    Time: 107 ms (22.63%), Space: 43.3 MB (46.72%) - LeetHub

--- 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.cs
+++ 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.cs
@@ -1,18 +1,5 @@
 public class Solution {
     public int MaximumCount(int[] nums) {
-       // return Math.Max(nums.Count(n=>n>0), nums.Count(n=>n<0));
-        
-        var p = 0;
-        var n = 0;
-        for(var i = 0; i<nums.Length; i++ ){
-            if(nums[i]<0){
-                n++;
-            }
-            if(nums[i]>0){
-                p++;
-            }
-        }
-        
-        return Math.Max(p, n);
+        return Math.Max(nums.Count(number=>number>0), nums.Count(number=>number<0));
     }
 }
