3a4437a646d7e15212f0c615afcfea6837331a04
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-05-26 11:14:00 +0300

    Time: 71 ms (82.50%), Space: 43.1 MB (37.50%) - LeetHub

--- 0696-count-binary-substrings/0696-count-binary-substrings.cs
+++ 0696-count-binary-substrings/0696-count-binary-substrings.cs
@@ -1,29 +1,26 @@
 public class Solution {
     public int CountBinarySubstrings(string s) {
-        
-        var prev =0;
-        var curr =1;
         var sum = 0;
+        var curr = 1;
+        var prev = 0;
         
-        for(var i = 1; i<s.Length; i++)
-        {
-            if (s[i] != s[i - 1])
-            {
-                sum += Math.Min(curr, prev); 
-                
-                prev = curr; 
-                
-                curr = 1;  
+        for(var i = 1; i<s.Length; i++ ){
+            
+            if(s[i] != s[i-1]){
+                sum += Math.Min(prev, curr);
+                prev = curr;
+                curr = 1;
             }
-            else
-            {
-                curr++; 
-  
+            else{
+               curr++; 
             }
         }
         
-        sum += Math.Min(curr,prev); // For the final group
+        sum += Math.Min(prev, curr);
         
         return sum;
     }
 }
+
+
+// 1
