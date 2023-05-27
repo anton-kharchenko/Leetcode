c64d5fe1afa356dcd24db8056ee83a1bfc6aa3f9
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-05-27 16:54:55 +0300

    Time: 103 ms (8.04%), Space: 39.7 MB (61.30%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        var profit = 0;
+        
+        for(var i =1; i<prices.Length; i++){
+            if(prices[i] > prices[i-1]){
+                profit += prices[i] - prices[i-1];
+            }
+        }
+        
+        return profit;
+    }
+}
