62b17ce27071ea13533ce44e2683107ac168b2ed
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-05-28 17:52:05 +0300

    Time: 355 ms (70.60%), Space: 40.8 MB (63.86%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cs
@@ -0,0 +1,41 @@
+public class Solution {
+    public bool Exist(char[][] board, string word) {
+         if(board == null || board[0].Length == 0)
+            return false;
+        if(word == ""){
+             return false;
+        }
+        
+        for(var i = 0; i<board.Length; i++){
+            for(var j = 0; j<board[0].Length; j++){
+                    if(Dfs(board, i, j, word, 0)){
+                        return true;
+                    }
+            }
+        }
+        
+        return false;
+    }
+    
+   bool Dfs(char[][] board, int row, int col, string word, int index) {
+       
+        if(index == word.Length){
+            return true;
+        }
+        
+        if( row < 0|| col < 0|| row >= board.Length || col >= board[0].Length || board[row][col] != word[index]){
+            return false;
+        }
+       
+        board[row][col] = '#';
+       
+        var res= Dfs(board, row + 1, col, word, index+1)
+            || Dfs(board, row - 1, col, word, index+1)
+            || Dfs(board, row, col+1, word, index+1)
+            || Dfs(board, row, col-1, word, index+1);
+       
+        board[row][col] = word[index];
+       
+       return res;
+    }
+}
