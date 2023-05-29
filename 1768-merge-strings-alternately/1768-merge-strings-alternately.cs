3ab4ebf8a6a177ac8a779eaad80eb26157c0d676
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-05-29 20:10:14 +0300

    Time: 92 ms (6.36%), Space: 39 MB (5.82%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public string MergeAlternately(string word1, string word2) {
+        var res = string.Empty;
+        
+        var count1 = 0;
+        var count2 = 0;
+        while(count1 != word1.Length && count2 != word2.Length){
+          if(count1 != word1.Length )  res += word1[count1++];
+          if(count2 != word2.Length )  res += word2[count2++];  
+        }
+        
+        while(count1 != word1.Length){
+            res += word1[count1++];
+        }
+        
+        while(count2 != word2.Length){
+            res += word2[count2++];
+        }
+        
+        return res;
+    }
+}
