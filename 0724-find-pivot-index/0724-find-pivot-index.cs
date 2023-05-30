f630682469ff8b79a2737c555a2d50ba33b5491c
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-05-30 14:01:54 +0300

    Time: 105 ms (79.89%), Space: 46.1 MB (47.85%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cs
+++ 0724-find-pivot-index/0724-find-pivot-index.cs
@@ -2,15 +2,14 @@ public class Solution {
     public int PivotIndex(int[] nums) {
         var left = 0;
         var right = nums.Skip(1).Sum();
-        if(left == right)
+        
+        if(left == right){
             return 0;
-        
-        
+        }
         
         for(var i = 1; i<nums.Length; i++){
-            left += nums[i - 1];
+            left += nums[i-1];
             right -= nums[i];
-            
             if(left == right){
                 return i;
             }
