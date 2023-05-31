1e4a9e696f9700d12edb4f7b6dc0fc2db2f96524
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-05-31 16:44:41 +0300

    Time: 102 ms (5.42%), Space: 38.3 MB (61.36%) - LeetHub

--- 1051-height-checker/1051-height-checker.cs
+++ 1051-height-checker/1051-height-checker.cs
@@ -1,9 +1,9 @@
 public class Solution {
     public int HeightChecker(int[] heights) {
 
-        var expected =  heights.ToList();
+        var expected =  heights.ToList(); // O(n)
         
-        expected.Sort();
+        expected.Sort(); // O(n log n)
         
         var count = 0;
         
@@ -11,9 +11,12 @@ public class Solution {
             if(heights[i] != expected[i]){
                 count++;
             }
-        }
+        } // O (n)
         
         return count;
         
     }
+    
+    //Time: O(n log n)
+    // Space O(2n) ~ O(n)
 }
