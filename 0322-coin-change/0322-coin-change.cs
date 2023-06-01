334d19d341aec183203572b135bf734eb930164c
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-06-01 14:45:53 +0300

    Time: 99 ms (82.20%), Space: 42 MB (65.45%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int CoinChange(int[] coins, int amount) {
+        if(amount == 0) return 0;
+        
+        int[] dp = new int[amount+1];
+        Array.Fill(dp, int.MaxValue);
+        dp[0] = 0;
+        
+        foreach(var coin in coins)
+        {
+            for(int i = coin; i <=amount; i++)
+            {
+                if(dp[i-coin] != int.MaxValue)
+                {
+                    dp[i] = Math.Min(dp[i], dp[i-coin] + 1);
+                }
+            }
+        }
+        
+        return dp[amount] == int.MaxValue ? -1 : dp[amount];
+    }
+}
