0e41f6a5c3bf6af679d0f719d6e80c95b3b11e6c
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-06-02 08:08:50 +0300

    Time: 253 ms (5.58%), Space: 52 MB (12.01%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cs
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cs
@@ -1,11 +1,6 @@
 public class Solution {
     public int FindKthLargest(int[] nums, int k)
     {
-        Array.Sort(nums);
-        if (nums.Length - k >= 0)
-        {
-            return nums[nums.Length - k];
-        }
-        return -1;
+       return nums.OrderBy(x=>x).ToArray()[^k];
     }
 }
