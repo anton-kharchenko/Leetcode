6dc0a42f8faf99575fd048be70b3b3a958e9c0a1
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-06-02 10:08:52 +0300

    Time: 79 ms (90.54%), Space: 38.3 MB (73.99%) - LeetHub

--- 1051-height-checker/1051-height-checker.cs
+++ 1051-height-checker/1051-height-checker.cs
@@ -2,23 +2,24 @@ public class Solution {
     public int HeightChecker(int[] heights) {
         var expected = new int[heights.Length];
         
-        Array.Copy(heights, expected, heights.Length);
+        Array.Copy(heights, expected, heights.Length);// O(n)
         
-        Array.Sort(expected); // O(n*logn)
+        Array.Sort(expected); // O(n* log n)
+        
         
         var result = 0;
-        
         for(var i = 0; i<heights.Length; i++){ // O(n)
+              
             if(heights[i] != expected[i]){
                 result++;
             }
         }
-        
         return result;
     }
 
 }
 
+// Time: O(n) + O(n* log n) +  O(n) = O(n* log n)
+// Space: O(n)
 
-// Time -  O(n log n)
-// Space - O(n)
+
