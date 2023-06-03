f93ef0c9224338bff27ff4ae5c3d31a0c5ebd507
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-06-03 22:35:32 +0300

    Time: 137 ms (69.74%), Space: 50.8 MB (31.28%) - LeetHub

--- 0561-array-partition/0561-array-partition.cs
+++ 0561-array-partition/0561-array-partition.cs
@@ -1,11 +1,13 @@
 public class Solution {
     public int ArrayPairSum(int[] nums) {
         Array.Sort(nums);
-        var res = 0;
-        for(var i = 0; i<nums.Length; i+=2){
-            res += nums[i];
+        
+        var sum = 0;
+        
+        for(var i = 0;i<nums.Length; i += 2){
+            sum += nums[i];
         }
         
-        return res;
+        return sum;
     }
 }
