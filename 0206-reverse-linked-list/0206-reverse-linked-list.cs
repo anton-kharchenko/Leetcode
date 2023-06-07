11c4ec4719f3645ca472ad1d09ad7469483a24f3
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-06-08 00:32:23 +0300

    Time: 83 ms (79.80%), Space: 39.1 MB (56.53%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cs
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cs
@@ -12,13 +12,13 @@
 public class Solution {
     public ListNode ReverseList(ListNode head) {
         ListNode reversed = null;
-        var curr = head;
+        var curr= head;
         
         while(curr!= null){
-         var next = curr.next;
-         curr.next = reversed;
-         reversed = curr;   
-         curr = next;
+            var next = curr.next;
+            curr.next = reversed;
+            reversed = curr;
+            curr = next;
         }
         
         return reversed;
