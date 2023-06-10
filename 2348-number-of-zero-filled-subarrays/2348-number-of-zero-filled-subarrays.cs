d50e2ac1c79ae1f09ab2fdb61ecb1eea9703f3c9
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-06-10 20:07:53 +0300

    Time: 267 ms (85.71%), Space: 54.1 MB (23.81%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cs
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cs
@@ -1,8 +1,8 @@
 public class Solution {
     public long ZeroFilledSubarray(int[] nums) {
-        long count = 0, result = 0;
+        long result = 0, count = 0;
         
-        for(var i = 0;i<nums.Length; i++){
+        for(var i = 0; i<nums.Length;i++){
             if(nums[i] == 0){
                 count += 1;
             }else{
