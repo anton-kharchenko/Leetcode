6278efe879f46e9d7aca4047e57fea1f353eed05
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-06-12 23:43:09 +0300

    Time: 94 ms (48.19%), Space: 45 MB (79.86%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cs
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cs
@@ -1,12 +1,22 @@
 public class Solution {
     public string ReverseWords(string s) {
-    var words  = s.Split(' ').ToArray();
-    var left = 0;
-    var res = string.Empty;
-    foreach(var word in words){
-        res += string.Concat(word.Reverse());
-        res += " ";
-    }
-    return res.Trim();
+        int count  = 0;
+        var sb = new StringBuilder(s);
+        
+        for(var i = sb.Length - 1;i>=0; i--){
+            count++;
+            if(sb[i] == ' ' || i==0){
+                    int k = i+count-1;
+                for(int j = i == 0 ? 0: i+1; j < k; j++) {
+                    (sb[j], sb[k]) = (sb[k], sb[j]);
+                    k--;
+                }
+       
+                  count = 0;
+            }
+          
+        }
+        
+        return sb.ToString();
     }
 }
