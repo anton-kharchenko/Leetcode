3859680822f525f21e0abd1b61ce2bea57f648f1
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-06-16 12:52:17 +0300

    Time: 185 ms (18.61%), Space: 50.5 MB (29.00%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cs
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cs
@@ -14,20 +14,22 @@
 public class Solution {
     int result = 0;
     public int GoodNodes(TreeNode root) {
-        if(root.left == null & root.right == null) return 1;
+            
         Dfs(root, root.val, root.val);
         return result;
     }
     
-    public void Dfs(TreeNode node, int rootVal, int prev){
-        if(node ==null) return;
+    public void Dfs(TreeNode node, int target, int prev){
+        if(node == null){
+            return; 
+        }
         
-        if(rootVal <= node.val && prev <=node.val){
+        if(node.val >= target && node.val >= prev){
             result++;
             prev = node.val;
         }
         
-        Dfs(node.left, rootVal, prev);
-        Dfs(node.right, rootVal, prev);
+        Dfs(node.left, target, prev);
+        Dfs(node.right, target, prev);
     }
 }
