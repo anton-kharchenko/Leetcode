ada7df0fa62a9fff6fcd83300420408439edf123
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-06-23 15:54:59 +0300

    Time: 1141 ms (31.34%), Space: 99.9 MB (20.90%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cs
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cs
@@ -1,18 +1,23 @@
 public class NumMatrix {
-    int[][] grid = null;
+    int[][] dp = null;
     public NumMatrix(int[][] matrix) {
-        grid = matrix;
+        if(matrix.Length == 0 || matrix[0].Length == 0) return ;
+        dp = new int[matrix.Length][];
+        
+        for(int i=0; i<matrix.Length; i++)
+        {
+            dp[i] = new int[matrix[0].Length + 1];
+            for(int j=0; j<matrix[0].Length; j++)
+            {
+                dp[i][j+1] = dp[i][j] + matrix[i][j];
+            }
+        }
     }
     
     public int SumRegion(int row1, int col1, int row2, int col2) {
-        var sum = 0;
-        while(row1<= row2){
-            for(var i = col1; i<=col2; i++ ){
-                sum += grid[row1][i];
-            }
-            
-            row1++;
-        }
+       int sum = 0;
+        for(int row = row1; row <= row2; row++)
+            sum += dp[row][col2 + 1] - dp[row][col1];
         
         return sum;
     }
