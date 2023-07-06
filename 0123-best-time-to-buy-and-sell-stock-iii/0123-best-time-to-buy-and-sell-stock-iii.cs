61c91ba97075f8a3c553f6bd753c07782baf5333
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-07-06 17:39:29 +0300

    Time: 230 ms (55.30%), Space: 50.6 MB (86.36%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cs
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cs
@@ -1,18 +1,14 @@
 public class Solution {
     public int MaxProfit(int[] prices) {
-        if(prices.Length == 0) return 0;
-        int firstBuy = int.MinValue;
-        int secondBuy = int.MinValue;
-        int firstSell = 0;
-        int secondSell = 0;
-        
-        for(var i = 0; i<prices.Length; i++){
-            firstBuy =  Math.Max(firstBuy, -prices[i]);
-            firstSell = Math.Max(firstSell, firstBuy + prices[i]);
-            secondBuy =  Math.Max(secondBuy, firstSell-prices[i]);
-            secondSell = Math.Max(secondSell, secondBuy + prices[i]);
-        }
-        
-        return secondSell;
+    if(prices.Length == 0) return 0;
+    int firstBuy = int.MinValue, secondBuy = int.MinValue, firstSell = 0, secondSell = 0;
+    foreach (var price in prices)
+    {
+        firstBuy =  Math.Max(firstBuy, -price); 
+        firstSell = Math.Max(firstSell, firstBuy + price); 
+        secondBuy =  Math.Max(secondBuy, firstSell-price); 
+        secondSell = Math.Max(secondSell, secondBuy + price); 
+    }
+    return secondSell;
     }
 }
