1cbe1f8d38d706260e5dfd875ddde9f128a5c2e5
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-07-21 19:43:00 +0300

    Time: 1819 ms (19.68%), Space: 62.8 MB (60.92%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cs
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cs
@@ -1,25 +1,32 @@
 public class Solution {
     public int[] ReplaceElements(int[] arr) {
-        if(arr.Length == 1){
-            return new []{-1};
-        }
+    if(arr.Length == 1){
+        return new int[]{-1};
+    }
+     for(var i = 0; i<arr.Length; i++){
+         if(i+1<arr.Length){
+              arr[i] = FindMax(arr, i+1, arr.Length);
+         }
         
-        for(var i = 0; i<arr.Length; i++ ){
-            arr[i] = FindMax(arr, i+1, arr.Length); 
-        }
+     }   
+       
         arr[^1] = -1;
         return arr;
     }
     
     public int FindMax(int[] arr, int start, int end){
-        var max = 0;
+        var max = arr[start];
+        
+        
         while(start<end){
-            if(arr[start] > max){
-                max = arr[start];
+            var curr = arr[start];
+            if(max<curr){
+                max = curr;
             }
             
             start++;
         }
+        
         
         return max;
     }
