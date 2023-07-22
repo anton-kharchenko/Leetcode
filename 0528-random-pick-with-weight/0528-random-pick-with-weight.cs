91aac5213b73b674492cd08898471f482da6d9d2
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-07-22 19:27:58 +0300

    Time: 204 ms (64.38%), Space: 60.1 MB (56.16%) - LeetHub

---
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    int[] weight;
+    Random _rnd = new  Random();
+    public Solution(int[] w) {
+        weight = new int[w.Length];
+        var max = 0;
+        for(var i = 0; i<w.Length; i++){
+            max += w[i];
+            weight[i] = max; 
+        }
+        
+    }
+    
+    public int PickIndex() {
+        var index = _rnd.Next(0, weight[^1]) + 1;
+        var res = Array.BinarySearch(weight, index);
+        return res >= 0 ? res : -res -1;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(w);
+ * int param_1 = obj.PickIndex();
+ */
