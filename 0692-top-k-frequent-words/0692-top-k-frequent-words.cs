e6b1b8a72e4a13c544e7aaa102bee3c23e472d1f
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-07-23 16:54:04 +0300

    Time: 182 ms (12.58%), Space: 48 MB (70.20%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cs
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cs
@@ -1,15 +1,13 @@
 public class Solution {
     public IList<string> TopKFrequent(string[] words, int k) {
-        var dict = new Dictionary<string, int>();
-        
+        var dictionary = new Dictionary<string, int>();
         foreach(var word in words){
-            if(dict.ContainsKey(word)){
-                dict[word]++;
+            if(dictionary.ContainsKey(word)){
+                dictionary[word]++;
             }else{
-                dict.Add(word, 1);
+                dictionary.Add(word, 1);
             }
         }
-        
-        return dict.OrderByDescending(i=>i.Value).ThenBy(i=>i.Key).Take(k).Select(i=>i.Key).ToArray();
+        return dictionary.OrderByDescending(i=>i.Value).ThenBy(i=>i.Key).Take(k).Select(i=>i.Key).ToList();
     }
 }
