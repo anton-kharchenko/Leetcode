6718876cc7ec6fa566ce0fbbd7e1b9feccc3865b
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-07-23 16:46:10 +0300

    Time: 334 ms (82.93%), Space: 40.7 MB (87.75%) - LeetHub

--- 0079-word-search/0079-word-search.cs
+++ 0079-word-search/0079-word-search.cs
@@ -1,36 +1,32 @@
 public class Solution {
     public bool Exist(char[][] board, string word) {
-        
         for(var i = 0; i<board.Length; i++){
             for(var j = 0; j<board[0].Length; j++){
-                    if(Dfs(board, i, j, word, 0)){
-                        return true;
-                    }
+                if(Dfs(i, j, board.Length, board[0].Length, word, board, 0)){
+                    return true;
+                }
             }
         }
         
         return false;
     }
     
-   bool Dfs(char[][] board, int row, int col, string word, int index) {
-       
-        if(index == word.Length){
-            return true;
-        }
+    public bool Dfs(int i, int j, int n, int m, string dest, char[][] board, int index){
+        if(index == dest.Length) return true;
         
-        if( row < 0|| col < 0|| row >= board.Length || col >= board[0].Length || board[row][col] != word[index]){
+        if(i<0|| j<0|| i>=n|| j>=m || dest[index] != board[i][j]){
             return false;
         }
-       
-        board[row][col] = '#';
-       
-        var res= Dfs(board, row + 1, col, word, index+1)
-            || Dfs(board, row - 1, col, word, index+1)
-            || Dfs(board, row, col+1, word, index+1)
-            || Dfs(board, row, col-1, word, index+1);
-       
-        board[row][col] = word[index];
-       
-       return res;
+        
+        board[i][j] = '#';
+        
+        var res = Dfs(i+1, j, n, m, dest, board, index+1) || 
+            Dfs(i-1, j, n, m, dest, board,  index+1) ||
+            Dfs(i, j+1, n, m, dest, board,  index+1) ||
+            Dfs(i, j-1, n, m, dest, board,  index+1);
+        
+        board[i][j] = dest[index];     
+        
+        return res;    
     }
 }
