69947c29dae1a3c2f6d5b6001a832cf4eca52df0
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-07-23 20:02:58 +0300

    Time: 176 ms (16.43%), Space: 46.4 MB (50.31%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cs
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cs
@@ -1,15 +1,15 @@
 public class Solution {
     public int[] TopKFrequent(int[] nums, int k) {
-        var dic = new Dictionary<int, int>();
-    
-        foreach(var num in nums){
-            if(dic.ContainsKey(num)){
-                dic[num] += 1;
+        var dict = new Dictionary<int, int>();
+        
+        foreach(var n in nums){
+            if(dict.ContainsKey(n)){
+                dict[n]++;
             }else{
-                dic.Add(num, 1);
+                dict.Add(n, 1);
             }
         }
         
-        return dic.OrderByDescending(i=>i.Value).Take(k).Select(i=>i.Key).ToArray();
+        return dict.OrderByDescending(i=>i.Value).Take(k).Select(i=>i.Key).ToArray();
     }
 }
