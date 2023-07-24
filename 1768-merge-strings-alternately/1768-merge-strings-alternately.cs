f9adba43234e2ace841ef173f4cf74c451d3d967
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-07-24 11:01:31 +0300

    Time: 92 ms (8.52%), Space: 39 MB (8.70%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cs
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cs
@@ -1,20 +1,21 @@
 public class Solution {
     public string MergeAlternately(string word1, string word2) {
-        var res = string.Empty;
-        
-        var count1 = 0;
+        var count1= 0;
         var count2 = 0;
-        while(count1 != word1.Length && count2 != word2.Length){
-          if(count1 != word1.Length )  res += word1[count1++];
-          if(count2 != word2.Length )  res += word2[count2++];  
+        var res = "";
+        
+        while(count1!= word1.Length && count2!= word2.Length){
+            if(count1!= word1.Length) res+= word1[count1++];
+            if(count2!= word2.Length) res+= word2[count2++];    
         }
         
-        while(count1 != word1.Length){
-            res += word1[count1++];
+        
+        while(count1!= word1.Length){
+            res+= word1[count1++];  
         }
         
-        while(count2 != word2.Length){
-            res += word2[count2++];
+        while(count2!= word2.Length){
+            res+= word2[count2++];  
         }
         
         return res;
