6158de9ce6a9f716a1c15d8992dff5f7502469e2
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-07-24 19:12:00 +0300

    Time: 94 ms, Memory: 44.9 MB - LeetHub

--- 0max-area-of-island/0max-area-of-island.cs
+++ 0max-area-of-island/0max-area-of-island.cs
@@ -1,12 +1,13 @@
 public class Solution {
     int max = 0;
+    
     public int MaxAreaOfIsland(int[][] grid) {
-        var visited = new HashSet<(int, int)>();
+        var set = new HashSet<(int, int)>();
         
         for(var i = 0; i<grid.Length; i++){
             for(var j = 0; j<grid[0].Length; j++){
-                if(!visited.Contains((i, j)) && grid[i][j] == 1){
-                  max = Math.Max(Dfs(grid, visited, i, j), max) ;
+                if(!set.Contains((i, j)) && grid[i][j] == 1){
+                    max = Math.Max(Dfs(i, j, grid.Length, grid[0].Length, set, grid), max);
                 }
             }
         }
@@ -14,17 +15,15 @@ public class Solution {
         return max;
     }
     
-    
-    public int Dfs(int[][] grid, HashSet<(int, int)> visited, int i, int j){
-        if(i<0 || j<0 || i>= grid.Length || j>= grid[0].Length || grid[i][j] == 0 || visited.Contains((i, j))){
+    public int Dfs(int i, int j, int n, int m, HashSet<(int, int)> set, int[][] grid){
+        if(i<0|| j<0 || i>=n || j>=m || set.Contains((i, j)) || grid[i][j] != 1){
             return 0;
         }
+        set.Add((i, j));
         
-        visited.Add((i,j));
-        
-        return 1 + Dfs(grid, visited, i+1, j)
-         + Dfs(grid, visited, i-1, j)
-         + Dfs(grid, visited, i, j+1)
-         + Dfs(grid, visited, i, j-1);
+        return 1+ Dfs(i+1, j, n, m, set, grid) 
+            + Dfs(i-1, j, n, m, set, grid) 
+            + Dfs(i, j+1, n, m, set, grid) 
+            + Dfs(i, j-1, n, m, set, grid);
     }
 }
