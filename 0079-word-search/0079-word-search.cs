7720da43576e797cd7447b619a6ca0dc7187cda3
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-07-24 07:11:56 +0300

    Time: 338 ms (81.38%), Space: 40.8 MB (77.13%) - LeetHub

--- 0079-word-search/0079-word-search.cs
+++ 0079-word-search/0079-word-search.cs
@@ -2,7 +2,7 @@ public class Solution {
     public bool Exist(char[][] board, string word) {
         for(var i = 0; i<board.Length; i++){
             for(var j = 0; j<board[0].Length; j++){
-                if(Dfs(i, j, board.Length, board[0].Length, word, board, 0)){
+                if(Dfs(i, j, board.Length, board[0].Length, board, word, 0)){
                     return true;
                 }
             }
@@ -11,22 +11,25 @@ public class Solution {
         return false;
     }
     
-    public bool Dfs(int i, int j, int n, int m, string dest, char[][] board, int index){
-        if(index == dest.Length) return true;
+    public bool Dfs(int i, int j, int n, int m, char[][] board, string word, int index){
+        if(index == word.Length){
+            return true;
+        }
         
-        if(i<0|| j<0|| i>=n|| j>=m || dest[index] != board[i][j]){
+        if(i<0 || j<0 || i>= n || j>=m || board[i][j] != word[index]){
             return false;
         }
         
         board[i][j] = '#';
         
-        var res = Dfs(i+1, j, n, m, dest, board, index+1) || 
-            Dfs(i-1, j, n, m, dest, board,  index+1) ||
-            Dfs(i, j+1, n, m, dest, board,  index+1) ||
-            Dfs(i, j-1, n, m, dest, board,  index+1);
+        var res = Dfs(i+1, j, n, m, board, word, index + 1) ||
+                  Dfs(i-1, j, n, m, board, word, index + 1) ||
+            Dfs(i, j+1, n, m, board, word, index + 1) ||
+            Dfs(i, j-1, n, m, board, word, index + 1);
         
-        board[i][j] = dest[index];     
+        board[i][j] = word[index];
         
-        return res;    
+        return res;
     }
+    
 }
