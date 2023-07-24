23f6db969bb2e50d7d82909f6aa3148e0796dc0a
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-07-24 12:19:06 +0300

    Time: 184 ms (19.87%), Space: 54.5 MB (26.59%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cs
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cs
@@ -1,26 +1,24 @@
 public class Solution {
     public int[] ProductExceptSelf(int[] nums) {
-        var length = nums.Length;
-        
-        var arr = new int[length];
-        var left = new int[length];
-        var right = new int[length];
+        var ans = new int[nums.Length];
+        var left = new int[nums.Length];
+        var right =  new int[nums.Length];
         
         Array.Fill(left, 1);
         Array.Fill(right, 1);
         
-        for(var i = 1; i<length; i++){
-            left[i] = left[i-1] * nums[i-1]; // Count left part 
+        for(var i = 1; i<nums.Length; i++){
+            left[i] = left[i-1] * nums[i-1];
         }
         
-        for(var i = length - 2; i>=0; i--){
-            right[i] = right[i+1] * nums[i+1]; // Count right part 
+        for(var i = nums.Length -2; i>=0; i--){
+            right[i] = right[i+1] * nums[i+1];
         }
         
-        for(var i = 0; i<length; i++){
-            arr[i] = left[i] * right[i]; // Multiply it 
+        for(var i = 0; i<nums.Length; i++){
+            ans[i] = left[i] * right[i];
         }
         
-        return arr;
+        return ans;
     }
 }
