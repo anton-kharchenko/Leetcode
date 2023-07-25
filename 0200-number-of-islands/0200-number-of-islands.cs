3387791d5ca95275fa23e500716eeb4d3023b314
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-07-25 11:58:26 +0300

    Time: 123 ms (55.94%), Space: 49.2 MB (69.12%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cs
+++ 0200-number-of-islands/0200-number-of-islands.cs
@@ -1,24 +1,22 @@
 public class Solution {
  public int NumIslands(char[][] grid) {
- var count  = 0;
- var n = grid.Length;
- var m = grid[0].Length;    
- for(var i=0; i<n; i++){
-     for(var j = 0; j<m; j++){
-         if(grid[i][j] == '1'){
-             Dfs(i, j, n, m, grid);
-             count++;
-         }
-     }
- } 
+     var n = grid.Length;
+     var m = grid[0].Length;
+     var res = 0;
+    for(var i = 0; i<n; i++){
+        for(var j = 0; j<m; j++){
+            if(grid[i][j] == '1'){
+                res++;
+                Dfs(i, j, n, m, grid);
+            }
+        }
+    }
      
-     
-     
- return count;
- }
+     return res;
+  }
     
     public void Dfs(int i, int j, int n, int m, char[][] grid){
-        if(i>=n || j>=m || i<0 || j<0 || grid[i][j] == '0'){
+        if(i<0 || j<0|| i>=n||j>=m || grid[i][j] == '0'){
             return;
         }
         
