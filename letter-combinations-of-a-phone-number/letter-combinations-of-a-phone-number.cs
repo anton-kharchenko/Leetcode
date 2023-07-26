82193328c07248ae6bfd00eb290cb6f612b39b15
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-07-26 18:27:27 +0300

    Time: 138 ms, Memory: 43.5 MB - LeetHub

---
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cs
@@ -0,0 +1,45 @@
+
+public class Solution
+{
+
+Dictionary<char, List<char>> letters = new   Dictionary<char, List<char>>();
+    
+    public IList<string> LetterCombinations(string digits) {
+        if(digits.Length == 0) return new List<string>();
+        
+        InitDict();
+        
+        return GetLetterCombinations(digits);
+    }
+    
+    private void InitDict()
+    {
+        letters.Add('1', new List<char>() { });
+        letters.Add('2', new List<char>() { 'a', 'b', 'c' });
+        letters.Add('3', new List<char>() { 'd', 'e', 'f' });
+        letters.Add('4', new List<char>() { 'g', 'h', 'i' });
+        letters.Add('5', new List<char>() { 'j', 'k', 'l' });
+        letters.Add('6', new List<char>() { 'm', 'n', 'o' });
+        letters.Add('7', new List<char>() { 'p', 'q', 'r', 's' });
+        letters.Add('8', new List<char>() { 't', 'u', 'v' });
+        letters.Add('9', new List<char>() { 'w', 'x', 'y', 'z' });
+    }
+    
+    public List<string> GetLetterCombinations(string digit){
+        if(digit.Length == 1)
+            return new List<string>(letters[digit[0]].Select(i=>i.ToString()));
+        
+        var res = new List<string>();
+        
+        var rest = GetLetterCombinations(digit.Substring(1)); 
+        
+        foreach(var i in letters[digit[0]]){
+            foreach(var r in rest){
+                res.Add(i+r);
+            }
+        }
+        
+        return res;
+    }
+    
+}
