9ebdf20f83db62e78d0a568ead0aa5726dad1f6a
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-07-27 13:16:19 +0300

    Time: 134 ms (100.00%), Space: 43.3 MB (42.68%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.cs
+++ 0399-evaluate-division/0399-evaluate-division.cs
@@ -1,45 +1,44 @@
 public class Solution {
-    Dictionary<string, List<(string dest, double cost)>> adj = new   Dictionary<string, List<(string dest, double cost)>>();
-    
+    Dictionary<string, List<(string des, double cost)>> adj = new Dictionary<string, List<(string des, double cost)>>();
+    HashSet<string> visited = new  HashSet<string> ();
     List<double> ans = new List<double>();
-    HashSet<string> visited = new HashSet<string> ();
-    
     public double[] CalcEquation(IList<IList<string>> equations, double[] values, IList<IList<string>> queries) {
+    
+    for(var i = 0; i<equations.Count; i++  ){
+        adj.TryAdd(equations[i][0], new List<(string des, double cost)>());
+         adj.TryAdd(equations[i][1], new List<(string des, double cost)>());
+        adj[equations[i][0]].Add((equations[i][1], values[i]));
+         adj[equations[i][1]].Add((equations[i][0], 1/values[i]));
+    }
         
-        for(var i = 0; i<equations.Count; i++){
-            adj.TryAdd(equations[i][0], new List<(string dest, double cost)>());
-            adj.TryAdd(equations[i][1], new List<(string dest, double cost)>());
-            adj[equations[i][0]].Add((equations[i][1], values[i]));
-            adj[equations[i][1]].Add((equations[i][0], 1/values[i]));
-        }
-        
-        foreach(var q in queries){
-            if(!adj.ContainsKey(q[0]) || !adj.ContainsKey(q[1])){
-                ans.Add(-1.0);
-                continue;
-            }
-            
-            var res = Dfs(q[0], q[1], 1.0);
-            ans.Add(res);
-            visited.Clear();
-        }
-        
-        
-        return ans.ToArray();
+     foreach(var q in queries){
+         if(!adj.ContainsKey(q[0]) || !adj.ContainsKey(q[1])){
+             ans.Add(-1.0);
+             continue;
+         }
+         
+         var res = Dfs( q[0], q[1], 1.0);
+         ans.Add(res);
+         visited.Clear();
+     }   
+    
+    return ans.ToArray();
     }
     
     
     public double Dfs(string start, string end, double prod){
         if(start == end) return prod;
         visited.Add(start);
+        
         foreach(var neigh in adj[start]){
-            if(!visited.Contains(neigh.dest)){
-                var res = Dfs(neigh.dest, end, prod * neigh.cost);
-                if(res>=0) return res;
+            if(!visited.Contains(neigh.des)){
+                var res = Dfs(neigh.des, end, prod * neigh.cost);
+                if(res >= 0) return res;
             }
         }
         
         return -1;
     }
+    
     
 }
