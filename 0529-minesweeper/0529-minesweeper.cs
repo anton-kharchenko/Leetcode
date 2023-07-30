0d9c777fa8922e19a3dc797ca5414937321aae39
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-07-30 21:57:47 +0300

    Time: 208 ms (100.00%), Space: 123.6 MB (12.50%) - LeetHub

---
+++ 0529-minesweeper/0529-minesweeper.cs
@@ -0,0 +1,44 @@
+public class Solution {
+    int[][] directions = new int[][]{new []{0,1}, new []{0,-1},  new []{1, 0}, new []{-1, 0}, new []{1,1}, new []{-1, -1}, new []{1, -1},new []{-1, 1}};
+    public char[][] UpdateBoard(char[][] board, int[] click) {
+        Dfs(board, click[0], click[1]);
+        return board;
+    }
+    
+    void Dfs(char[][]board, int r, int c){
+      if(r < 0 || r >= board.Length || c < 0 || c >= board[0].Length || board[r][c] == 'B')
+        return;  
+        
+        if(board[r][c] == 'M'){
+            board[r][c] = 'X';
+            return;
+        }
+        var mineCount = GetMineCount(board, r, c);
+        
+        if(mineCount>0){
+             board[r][c] = (char)('0'+mineCount);
+            return;
+        }
+        
+        if(mineCount  == 0){
+            board[r][c] = 'B';
+            foreach(var dir in directions){
+                int row = dir[0] + r;
+                int col = dir[1] + c;
+                Dfs(board, row, col);
+            }
+        }
+    }
+    
+    int GetMineCount(char[][]board, int r, int c){
+        int count = 0;
+        foreach(var dir in directions){
+            int row = dir[0] + r;
+            int col = dir[1] + c;
+            if(row >= 0 && row < board.Length && col >= 0 && col < board[0].Length && board[row][col] == 'M')
+                count++;
+        }
+        
+        return count;
+    }
+}
