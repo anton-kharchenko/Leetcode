acc05f16b398984fc93153be2cc4a97066e75ea2
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-08-01 09:02:31 +0300

    Time: 149 ms (98.16%), Space: 52.4 MB (43.33%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cs
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cs
@@ -1,28 +1,30 @@
 public class Solution {
     public int LongestConsecutive(int[] nums) {
-    if(nums.Length == 0) return 0;
-    if(nums.Length == 1) return 1;
-			
-    Array.Sort(nums);
+        if(nums.Length == 0) return 0;
+        if(nums.Length == 1) return 1;
         
-    var count = 0;
-    var maxCount = 0;
+        Array.Sort(nums); // O(nlogn)
         
-    for(var i = 0; i < nums.Length -1; i++){
+        int max = 0, count = 0;
         
-        if(nums[i+1] - nums[i] == 1){
-            count++;
+        for(var i = 0; i<nums.Length - 1; i++){
+            var curr = nums[i];
+            var nextNumber = nums[i+1];
+            
+            if(nextNumber - curr == 1){
+                count++;
+            }
+            
+            if(nextNumber - curr> 1){
+                count = 0;
+            }
+            
+            max = Math.Max(max, count);
         }
-
-        if (nums[i+1] - nums[i] > 1)
-        {
-            count = 0;
-        }
         
-        maxCount = Math.Max(maxCount, count);
         
-    }
-		
-    return maxCount + 1;
+        return max + 1;
     }
 }
+  // [100,4,200,1,3,2]
+  // 1,2,3,4,100
