e14c3a1902fb04d945a1c88337623b28bb60aeac
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-08-01 14:04:41 +0300

    Time: 142 ms (97.24%), Space: 43.8 MB (27.62%) - LeetHub

--- 0068-text-justification/0068-text-justification.cs
+++ 0068-text-justification/0068-text-justification.cs
@@ -1,71 +1,68 @@
 public class Solution {
     public IList<string> FullJustify(string[] words, int maxWidth) {
-        IList<string> retval = new List<string>();
+        var res = new List<string>();
+        var remainingList = new List<string>();
+        var subSetLength = 0;
+        var wordLength = 0;
         
-        List<string> lwords = new List<string>();
-        int sublen = 0;
-        int words_len = 0;
-        foreach(string word in words){
-            sublen += word.Length + 1;
-            //'+1' is for the space added above
-            if(sublen > maxWidth + 1){ 
-                string line = BuildLine(lwords, maxWidth, words_len);
-                retval.Add(line);
-                lwords.Clear();
-                //Remember to add back in the current word
-                sublen = word.Length + 1;
-                words_len = 0;
+        foreach(var word in words){
+            subSetLength += word.Length + 1;
+            
+            if(subSetLength> maxWidth + 1){
+                var line = BuildLine(remainingList, wordLength,  maxWidth);
+                res.Add(line);
+                remainingList.Clear();
+                subSetLength = word.Length + 1;
+                wordLength = 0;
             }
-            lwords.Add(word);     
-            words_len += word.Length;       
+            
+            remainingList.Add(word);
+            wordLength += word.Length; 
         }
         
-        //Do any remaining words
-        if(lwords.Count() > 0 ){
-            string line = "";
-            foreach(string word in lwords){
-                line += $"{word} ";
+        if(remainingList.Any()){
+            var line = "";
+            foreach(var w in remainingList){
+                line += $"{w} ";
             }
-            //NOTE: 'Trim()' is to remove the space on the last word
-            line = line.Trim().PadRight(maxWidth);
-            retval.Add(line);
+            
+           line = line.Trim().PadRight(maxWidth);
+            res.Add(line);
         }
         
-        return retval;
+        return res;
     }
     
-    private string BuildLine(List<string> words, int maxWidth, int words_len){
-        int word_count = words.Count();
-        int spaces = maxWidth - words_len;
+    public string BuildLine(List<string> words, int wordLength, int maxWidth){
+        var wordCount = words.Count;
+        var totalSpaces = maxWidth - wordLength;
         
-        //Special Cases: Handle a single or double word
-        if(word_count == 1){
+        if(wordCount == 1)
             return words[0].PadRight(maxWidth);
-        }else if (word_count == 2){
-            return words[0] + "".PadRight(spaces) + words[1];
-        }
+        if(wordCount == 2)
+            return words[0] + "".PadRight(totalSpaces) + words[1];
         
-        StringBuilder sb = new StringBuilder();
+        var sb = new StringBuilder();
         
-        //Number of space blocks
-        int num_of_spaces = word_count - 1;
-        //spaces per space block
-        int spaces_per_word = spaces / num_of_spaces;
-        //Extra spaces
-        int extra = spaces % num_of_spaces;
+        var numberOfSpaces = wordCount -1;
+        var spacesPerWord = totalSpaces / numberOfSpaces;
+        var extra = totalSpaces % numberOfSpaces;
         
-        for(int i = 0; i < word_count; i++){
-            string word = words[i];
-            sb.Append(word);
-            //Append spaces except for the last word
-            if( i != word_count - 1)
-                sb.Append(' ',spaces_per_word);
-            //Check for extra spaces
-            if( extra > 0 ){
-                sb.Append(' ',1);
+        for(var i = 0; i<words.Count; i++){
+            sb.Append(words[i]);
+            
+           if(i != wordCount - 1){
+                sb.Append(' ', spacesPerWord);
+            }
+            if(extra>0
+              ){
+                sb.Append(' ', 1);
                 extra--;
             }
         }
+        
         return sb.ToString();
+        
     }
+    
 }
