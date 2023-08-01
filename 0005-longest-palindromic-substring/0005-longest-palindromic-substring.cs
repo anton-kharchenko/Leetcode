5ebbde18cd6b792dbc566bca8193a3bfdb3ae4fe
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-08-01 11:08:21 +0300

    Time: 70 ms (99.29%), Space: 37.3 MB (98.08%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cs
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cs
@@ -1,25 +1,25 @@
 public class Solution {
     public string LongestPalindrome(string s) {
-       var start = 0;
-       var end = 0;
-        var len = 0;
+    
+        int start = 0;
+        int end = 0;
+        
         for(var i = 0; i<s.Length; i++){
-            len = Math.Max(ExpandSubstring(i,i, s), ExpandSubstring(i,i+1, s));
-            if(len>end-start){
-                start = i - (len - 1) / 2;
-                end = i + len / 2;
-            }
+            var len = Math.Max(GetLength(i, i, s), GetLength(i, i+1, s));
+            if(len<= end - start) continue;
+            start = i - (len - 1) / 2;
+            end = i + len/2;
         }
-
+        
         return s.Substring(start, end - start + 1);
     }
-
-    private int ExpandSubstring(int left, int right, string s){
-        while(left>=0&&right<s.Length&&s[left] == s[right]){
-            right++;
-            left--;
+    
+    public int GetLength(int start, int end, string s){
+        while(start>= 0  && end<s.Length && s[start] == s[end]){
+            start--;
+            end++;
         }
-
-        return right - left - 1;
+        
+        return end - start - 1;
     }
 }
