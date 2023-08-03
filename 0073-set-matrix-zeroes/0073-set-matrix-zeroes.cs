e63de9c1c61eb821e10ee9dccb693f2902e57061
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-08-03 10:56:03 +0300

    Time: 150 ms (85.82%), Space: 51.1 MB (7.02%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cs
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cs
@@ -1,39 +1,36 @@
 public class Solution {
+    int n = 0;
+    int m = 0;
     public void SetZeroes(int[][] matrix) {
-        var visited = new List<List<bool>>();
+        var visited = new HashSet<(int, int)>();
+        n = matrix.Length;
+        m = matrix[0].Length;
         
-        for(var i = 0; i<matrix.Length; i++){
-            visited.Add(new List<bool>());
-            
-            for(var j = 0; j< matrix[0].Length;j++){
-                visited[i].Add(false);
+        for(var i = 0;i<n; i++){
+            for(var j = 0;j<m; j++){
+                if(!visited.Contains((i, j)) && matrix[i][j] == 0){
+                    Dfs(i, j, matrix, visited);
+                }
             }
         }
+    
+    }
+    
+    public void Dfs(int row, int col, int[][] matrix, HashSet<(int, int)> visited){
         
-         for(var i = 0; i<matrix.Length; i++){
-             for(var j = 0; j< matrix[0].Length;j++){
-                 if(!visited[i][j] && matrix[i][j] == 0){
-                     Dfs(matrix, visited, i, j);
-                 }
-             }
+        visited.Add((row, col));
+        
+        for(var i = 0;i< m; i++ ){
+            if(visited.Contains((row, i)) || matrix[row][i] == 0) continue;
+            matrix[row][i] = 0;
+            visited.Add((row, i));
+        }
+        
+        for(var j = 0;j< n; j++ ){
+            if(visited.Contains((j, col)) || matrix[j][col] == 0) continue;
+            matrix[j][col] = 0;
+            visited.Add((j, col));
         }
     }
     
-    public void Dfs(int[][] matrix, List<List<bool>> visited, int row, int col){
-        visited[row][col] = true;
-        
-        for(var i = 0; i<matrix[0].Length; i++){
-            if(!visited[row][i] && matrix[row][i] != 0){
-                matrix[row][i] = 0;
-                visited[row][i] = true;
-            }
-        }
-        
-        for(var i = 0; i<matrix.Length; i++){
-            if(!visited[i][col] && matrix[i][col] != 0){
-                matrix[i][col] = 0;
-                visited[i][col] = true;
-            }
-        }
-    }
 }
