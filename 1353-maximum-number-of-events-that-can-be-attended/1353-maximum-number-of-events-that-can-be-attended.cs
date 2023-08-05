99175ff26bf868c033a1a89c41958457c7f77975
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-08-05 18:36:00 +0300

    Time: 440 ms (10.81%), Space: 69.4 MB (86.49%) - LeetHub

--- 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.cs
+++ 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.cs
@@ -1,22 +1,37 @@
 public class Solution {
     public int MaxEvents(int[][] events) {
-        events = events.OrderBy(x => x[1]).ThenBy(x => x[0]).ToArray();
-        var calendar = new int[events.Last()[1] + 1];
-        var prevStartDay = -1;
-        var prevBookDay = -1;
-        foreach(var evt in events) {
-            var startDay = evt[0];
-            if (prevStartDay == evt[0]) startDay = prevBookDay + 1;
-            prevStartDay = evt[0];
-            for(var i = startDay; i <= evt[1] && i < calendar.Length; i++) {
-                if (calendar[i] == 0) {
-                    calendar[i] = 1;
-                    prevBookDay = i;
+       // Sort the events by the end time and in case of tie by the start time in ascending order.
+        Array.Sort(events, (a, b) => {
+            if (a[1] == b[1]) {
+                return a[0] - b[0];
+            }
+            return a[1] - b[1];
+        });
+        
+        var calendar = new HashSet<int>();
+        var lastEventEnd = events[events.Length -1][1];
+        var prevStart = -1;
+        var lastSlotBooked = -1;
+        
+        foreach (var day in events) {
+            var start = day[0];
+            var end = day[1];
+            var calendarStart = start;
+            
+            // If the prev and current start at the same time, advance start by the last booked time
+            if (prevStart == calendarStart) {
+                calendarStart = lastSlotBooked + 1;
+            } 
+            for (var iter = calendarStart; iter <= end && iter <= lastEventEnd; ++iter) {
+                if (!calendar.Contains(iter)) {
+                    calendar.Add(iter);
+                    lastSlotBooked = iter;
                     break;
                 }
             }
+            prevStart = start;            
         }
-        //Console.WriteLine(string.Join(",", calendar));
-        return calendar.Count(x => x != 0);
+        
+        return calendar.Count;
     }
 }
