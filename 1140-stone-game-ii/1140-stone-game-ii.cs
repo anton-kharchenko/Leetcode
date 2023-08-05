a2bf9492154a79c9c149005abfcac700d1ff074c
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-08-05 17:07:51 +0300

    Time: 72 ms (100.00%), Space: 39.6 MB (72.22%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cs
+++ 1140-stone-game-ii/1140-stone-game-ii.cs
@@ -5,27 +5,30 @@ public class Solution
         var dp = new int[piles.Length, piles.Length];
 
         for (int i = piles.Length - 2; i >= 0; i--)
-            piles[i] += piles[i + 1];
+            piles[i] += piles[i + 1]; // Prefix sum : [2,7,9,4,4]  => [26,24,17,8,4]
 
-        return StoneGameIIUntil(piles, dp, 0, 1);
+        return StoneGameIiUntil(piles, dp, 0, 1);
     }
 
-    private int StoneGameIIUntil(int[] pilesSuffixSum, int[,] dp, int idx, int M)
+    private int StoneGameIiUntil(int[] pilesSuffixSum, int[,] dp, int idx, int m)
     {
-        int max_stones = 0, moves_possible = Math.Min(pilesSuffixSum.Length, 2 * M + idx);
+        int maxStones = 0, movesPossible = Math.Min(pilesSuffixSum.Length, 2 * m + idx); // Min possible move for player
 
-        if (moves_possible >= pilesSuffixSum.Length)
+        if (movesPossible >= pilesSuffixSum.Length)
             return pilesSuffixSum[idx];
 
-        if (dp[idx, M] != 0)
-            return dp[idx, M];
+        if (dp[idx, m] != 0)
+            return dp[idx, m];
 
-        for (int i = idx; i < moves_possible; i++)
+        for (int i = idx; i < movesPossible; i++)
         {
-            max_stones = Math.Max(max_stones, pilesSuffixSum[idx] - StoneGameIIUntil(pilesSuffixSum, dp, i + 1, Math.Max(i - idx + 1, M)));
+            var current = pilesSuffixSum[idx] - StoneGameIiUntil(pilesSuffixSum, dp, i + 1, Math.Max(i - idx + 1, m));
+            
+            maxStones = Math.Max(maxStones, current);
         }
 
-        dp[idx, M] = max_stones;
-        return dp[idx, M];
+        dp[idx, m] = maxStones;
+        
+        return dp[idx, m];
     }
 }
