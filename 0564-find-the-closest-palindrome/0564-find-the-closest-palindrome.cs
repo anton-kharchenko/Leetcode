bf6d92f232dafcf31e0aa565bacf8c9f3a5cc795
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-08-06 10:04:36 +0300

    Time: 78 ms (85.71%), Space: 37.9 MB (71.43%) - LeetHub

--- 0564-find-the-closest-palindrome/0564-find-the-closest-palindrome.cs
+++ 0564-find-the-closest-palindrome/0564-find-the-closest-palindrome.cs
@@ -1,34 +1,38 @@
-public class Solution 
+public class Solution
 {
-public string NearestPalindromic(string n)
-{
-var input = Convert.ToInt64(n);
-if (input <= 11)
-return (input < 11 ? input - 1 : input - 2).ToString();
-var spld = n.Substring(0, n.Length / 2 + n.Length % 2);
-var Pld = n.Substring(0, n.Length / 2) + new string(spld.Reverse().ToArray());
-var lowPld = Convert.ToInt64(Pld.ToString());
-var upPld = Convert.ToInt64(Pld.ToString());
-
-        if (upPld.ToString().Equals(n) || upPld < input)
-        {
-            var upnum = (Convert.ToInt64(spld) + 1).ToString();
-            var upinc = upnum.Length > spld.Length ? n.Length % 2 + 1 : n.Length % 2;
-            Pld = upnum + new string(upnum.Substring(0, upnum.Length - upinc).Reverse().ToArray());
-            upPld = Convert.ToInt64(Pld.ToString());
-        }
-        if (lowPld.ToString().Equals(n) || lowPld > input)
-        {
-            var lownum = (Convert.ToInt64(spld) - 1).ToString();
-            var lowinc = n.Length % 2;
-            if (lownum.Length < spld.Length)
-            {
-                lownum = lownum + lownum.Last();
-                lowinc = lowinc + 1;
+    public string NearestPalindromic(string n)
+    {
+        int length = n.Length;
+        int left = (length + 1) / 2, right = length - left;
+        long head = long.Parse(n.Substring(0, left));
+        long origin = long.Parse(n);
+        long diff = long.MaxValue;
+        long num = 0;
+        
+        for(var i = -1; i<= 1; i++ ){
+            var ret = GetPalindrom(head + i, right);
+            if(ret != origin && Math.Abs(ret - origin)<diff){
+                diff = Math.Abs(ret - origin);
+                num = ret;
             }
-            Pld = lownum + new string(lownum.Substring(0, lownum.Length - lowinc).Reverse().ToArray());
-            lowPld = Convert.ToInt64(Pld.ToString());
         }
-        return (input - lowPld) <= (upPld - input) ? lowPld.ToString() : upPld.ToString();
-}
+        
+        return num.ToString();
+    }
+    
+    public long GetPalindrom(long head, int rightLength){
+        var str = head.ToString();
+        var sb = new StringBuilder(str);
+        
+        
+        if(head == 0 || rightLength> str.Length){
+            for(var i = 0; i<rightLength; i++)
+                sb.Append('9');
+        }else{
+            for (int i = rightLength - 1; i >= 0; i--)
+                sb.Append(str[i]);
+        }
+        
+        return long.Parse(sb.ToString());
+    }
 }
