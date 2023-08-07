cb6c2e8c02ec4724d27d67ca0c21476b69a876c0
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-08-07 19:07:46 +0300

    Time: 3347 ms (9.86%), Space: 27.4 MB (9.86%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cs
+++ 0029-divide-two-integers/0029-divide-two-integers.cs
@@ -1,15 +1,15 @@
 public class Solution {
     public int Divide(int dividend, int divisor) {
-       if(dividend == int.MinValue && divisor == -1) return  int.MaxValue;
+        if(dividend == int.MinValue && divisor == -1) return  int.MaxValue;
         var sign = dividend> 0 ^ divisor> 0 ? -1 : 1;
         var res = 0;
-        long m = Math.Abs((long) dividend);
-        long n = Math.Abs((long) divisor);
+        var m = Math.Abs((long) dividend);
+        var n = Math.Abs((long) divisor);
         
         
         while(m>=n){
             long subN = n;
-            for(var i = 1; m>= subN; i<<=1, subN<<=1){
+            for(var i = 1; m>= subN;){
                 m -= subN;
                 res += i;
             }
