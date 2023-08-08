c7a0f372c4479df9260032f8688b3fa7ec757c8b
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-08-08 12:44:40 +0300

    Time: 147 ms (80.30%), Space: 43.5 MB (81.31%) - LeetHub

--- 0068-text-justification/0068-text-justification.cs
+++ 0068-text-justification/0068-text-justification.cs
@@ -1,72 +1,75 @@
 public class Solution {
     public IList<string> FullJustify(string[] words, int maxWidth) {
-        var res = new List<string>();
-        var temp = new List<string>();
+        var ans = new List<string>();
         var wordLength = 0;
-        var currentLength = 0;
+        var temp = new List<string>();
+        var count = 0;
         
         foreach(var word in words){
-            currentLength += word.Length + 1;
-            if(currentLength > maxWidth + 1){
-                var line =  BuildLine(temp, maxWidth, wordLength);
-                res.Add(line);
+            count += word.Length + 1;
+            
+            if(count>maxWidth+1){
+                var line = Builder(temp, maxWidth, wordLength);
+                ans.Add(line);
                 temp.Clear();
-                currentLength = word.Length + 1;
+                count = word.Length + 1;
                 wordLength = 0;
             }
-
-            wordLength += word.Length;
+            
             temp.Add(word);
+            wordLength += word.Length;
         }
-        
         
         if(temp.Any()){
             var line = "";
-            
             foreach(var i in temp){
                 line += $"{i} ";
             }
             
-            line = line.Trim().PadRight(maxWidth);
-            res.Add(line);
+          line = line.Trim().PadRight(maxWidth, ' ');
+          ans.Add(line);
         }
         
-        return res;
+        
+        
+        return ans;
     }
     
-    public string BuildLine(List<string> words, int maxWidth, int wordLength){
-        
-        var spaces = words.Count - 1;
-        var totalSpaces = maxWidth - wordLength;
+    
+    public string Builder(List<string> words,  int maxWidth, int wordLength){
+        var length = words.Count;
+        var spaces = words.Count  - 1 ;
+        var totalSpaces = maxWidth -  wordLength;
 
         
-        if(words.Count == 1)
-            return words[0].PadRight(maxWidth);
+        if(length == 1)
+            return words[0].PadRight(maxWidth, ' ');
         
-        if(words.Count == 2)
-            return words[0] + "".PadRight(totalSpaces)  + words[1];
-        
-        var countSpacesBetweenWords  = totalSpaces / spaces;
-        var extra = totalSpaces % spaces;
+        if(length == 2)
+            return words[0]  + "".PadRight(totalSpaces, ' ') + words[1];
         
         var sb = new StringBuilder();
         
-        for(var i = 0; i<words.Count; i++){
-            sb.Append(words[i]);
+        var spacesPerWord = totalSpaces / spaces;
+        var extra = totalSpaces % spaces; 
+        
+        for(var i = 0; i<length; i++){
+            var word = words[i];
             
-            if(i != words.Count - 1){
-             sb.Append(' ', countSpacesBetweenWords);
+          
+            
+            sb.Append(word);
+            
+            if(i<length - 1){
+                sb.Append(' ', spacesPerWord);
             }
             
             if(extra>0){
                 sb.Append(' ', 1);
-                extra --;
+                extra--;
             }
-            
         }
         
         return sb.ToString();
-        
     }
-    
 }
