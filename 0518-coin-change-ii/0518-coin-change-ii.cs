4d447a3e4f6e3fbe2294321fe1a30729f0df7608
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-08-10 19:16:03 +0300

    Time: 94 ms (52.41%), Space: 38.5 MB (81.33%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cs
+++ 0518-coin-change-ii/0518-coin-change-ii.cs
@@ -1,16 +1,18 @@
 public class Solution {
     public int Change(int amount, int[] coins) {
-    int[] d = new int[amount + 1];
-	d[0] = 1;
+        int[] d = new int[amount + 1];
+	    d[0] = 1;
 
 	foreach(var coin in coins) // Go through all exists coins
 	{
 		for(int a = coin; a <= amount; a++) // Calculate for every coin posible ways 
 		{                
-			d[a] += d[a - coin];                                   
+			d[a] += d[a - coin];            // 5                      
 		}
 	}
 
 	return d[amount];
     }
+    
+ 
 }
