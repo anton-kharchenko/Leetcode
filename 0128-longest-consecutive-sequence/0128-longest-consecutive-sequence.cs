c745f43f91984e7dbb34606b2ceb8b4db0d27cb4
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-08-12 09:35:59 +0300

    Time: 275 ms (34.11%), Space: 50.8 MB (65.89%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cs
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cs
@@ -3,28 +3,19 @@ public class Solution {
         if(nums.Length == 0) return 0;
         if(nums.Length == 1) return 1;
         
-        Array.Sort(nums); // O(nlogn)
-        
-        int max = 0, count = 0;
-        
-        for(var i = 0; i<nums.Length - 1; i++){
-            var curr = nums[i];
-            var nextNumber = nums[i+1];
-            
-            if(nextNumber - curr == 1){
-                count++;
+        var set = new HashSet<int>(nums);
+        var longest = 0;
+        foreach(var n in nums){
+           
+            if(!set.Contains(n - 1)){
+                 var length = 0;
+                 while(set.Contains(n + length)){
+                     length++;
+                 }
+                 longest = Math.Max(longest, length);
             }
-            
-            if(nextNumber - curr> 1){
-                count = 0;
-            }
-            
-            max = Math.Max(max, count);
         }
         
-        
-        return max + 1;
+        return longest;
     }
 }
-  // [100,4,200,1,3,2]
-  // 1,2,3,4,100
