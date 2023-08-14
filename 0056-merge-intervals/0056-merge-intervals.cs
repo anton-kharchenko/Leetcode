8bef05faeba0c25ec9ce8e16c43c35b7ddb6174c
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-08-14 11:14:49 +0300

    Time: 156 ms (90.02%), Space: 53.3 MB (12.77%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cs
+++ 0056-merge-intervals/0056-merge-intervals.cs
@@ -1,31 +1,25 @@
 public class Solution {
     public int[][] Merge(int[][] intervals) {
+        intervals = intervals.OrderBy(i=>i[1]).OrderBy(i=>i[0]).ToArray();
+        var start = int.MaxValue;
+        var end = 0;
+        var ans = new List<int[]>();
+        var n = intervals.Length;
         
-        intervals = intervals.OrderBy(a => a[1]).OrderBy(a => a[0]).ToArray();
         
-        int start = int.MaxValue;
-        
-        int end = 0;
-        
-        List<int[]> result = new List<int[]>();
-        
-        int count = intervals.Length;
-        
-        for (int i = 0; i < count; i++) {
+        for(var i = 0; i<n; i++){
+            start = Math.Min(intervals[i][0], start);
+            end = Math.Max(intervals[i][1], end);
             
-            start = Math.Min(start, intervals[i][0]);
-            
-            end = Math.Max(end, intervals[i][1]);
-            
-            if (i == count-1 || end < intervals[i+1][0]) {
-                result.Add(new int[2] { start, end });
-                
+            if(i == n-1 || end < intervals[i+1][0]){
+                ans.Add(new int[2]{start, end});
                 start = int.MaxValue;
-                
                 end = 0;
             }
+            
         }
         
-        return result.ToArray();
+        
+        return ans.ToArray();
     }
 }
