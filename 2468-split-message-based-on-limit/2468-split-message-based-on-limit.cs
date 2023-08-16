cb7d8dbba0a803a568e072d34677d2a9e9e44e51
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-08-16 12:44:25 +0300

    Time: 417 ms (100.00%), Space: 128.6 MB (50.00%) - LeetHub

--- 2468-split-message-based-on-limit/2468-split-message-based-on-limit.cs
+++ 2468-split-message-based-on-limit/2468-split-message-based-on-limit.cs
@@ -3,28 +3,32 @@ public string[] SplitMessage(string message, int limit)
     {
         var messageLength = message.Length;
         var parts = 1;
-        var allParts = Length(parts);
+        var allParts = Length(parts);   
         var ans = new List<string>();
     
-        while(parts * (3 + Length(parts)) + allParts + messageLength > limit*parts){
-            if((3+Length(parts) * 2) >=  limit)    return ans.ToArray();   
+        while(parts*(3 + Length(parts)) + allParts + messageLength > limit * parts){
+            if((3 + Length(parts)* 2)  >= limit){
+                return ans.ToArray(); 
+            }
             parts++;
             allParts += Length(parts);
         }
-        
-        for(var i = 1; i<= parts; i++){
-            var textLength = Math.Min(message.Length, limit - (3+ Length(i) + Length(parts)));
+    
+    
+        for(var i = 1; i<=parts; i++){
+            
+            var textLength = Math.Min(message.Length, limit - (3 + Length(i) + Length(parts)));
             var chunk = message.Substring(0, textLength);
             message = message.Substring(textLength);
-            
             ans.Add($"{chunk}<{i}/{parts}>");
         }
-        
+    
+    
         return ans.ToArray();
-        
     }
     
-    public int Length(int m){
-        return m.ToString().Length;
+    
+    public int Length(int value){
+        return value.ToString().Length;
     }
 }
