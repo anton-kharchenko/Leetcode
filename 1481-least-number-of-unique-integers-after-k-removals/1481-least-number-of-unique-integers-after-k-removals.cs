d5b7e9186295b5e0c078475b4c1364a516a19f18
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-08-27 11:23:11 +0300

    Time: 160 ms (96.92%), Space: 51.8 MB (100.00%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cs
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cs
@@ -1,34 +1,33 @@
 public class Solution {
     public int FindLeastNumOfUniqueInts(int[] arr, int k) {
-        var dictionary = new Dictionary<int, int>();
+            var dict = new Dictionary<int, int>();
+            foreach(var i in arr){
+                if(dict.ContainsKey(i)){
+                    dict[i]++;
+                }else{
+                    dict.Add(i, 1);
+                }
+            }
         
-        foreach(var item in arr){
-            if(dictionary.ContainsKey(item)){
-                dictionary[item]++;
-            }else{
-                dictionary.Add(item, 1);
-            }
-        }
-
-        var list = dictionary.Values.ToList();
-        list.Sort();
-        int count = 0;
-
-        for (var i = 0; i<list.Count; i++)
-        {
-            var current = list[i];
-
-            if (k >= current)
-            {
-                k -= current;
-            }
-            else
-            {
-                count = list.Count - i;
-                break;
-            }
-        }
+            var list = dict.Values.ToList();
+            list.Sort();
         
-        return count;
+            int count = 0;
+            
+            for(var i = 0; i<list.Count; i++){
+                var curr = list[i];
+                
+                if(k>=curr){
+                    k -= curr;
+                }else{
+                    count = list.Count  - i;
+                    break;
+                }
+                
+            }
+        
+        
+            return count;
+        
     }
 }
