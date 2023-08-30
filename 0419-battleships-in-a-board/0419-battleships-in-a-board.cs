cb32a295e556eb8dda5291b4625494db80ca958a
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-08-30 11:54:37 +0300

    Time: 80 ms (87.21%), Space: 40.9 MB (48.84%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.cs
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.cs
@@ -1,16 +1,16 @@
 public class Solution {
     int ans = 0;
-    int n = 0;
     int m = 0;
+    int n = 0;
     public int CountBattleships(char[][] board) {
-        n = board.Length;
-        m = board[0].Length;
+        m = board.Length;
+        n = board[0].Length;
         
-        for(var i = 0; i<n; i++){
-            for(var j = 0; j<m; j++){
+        for(var i = 0; i<m; i++){
+            for(var j = 0; j<n; j++){
                 if(board[i][j] == 'X'){
-                    Dfs(i, j, board);
                     ans++;
+                    Dfs(i, j, board);
                 }
             }
         }
@@ -18,15 +18,15 @@ public class Solution {
         return ans;
     }
     
-    public void Dfs(int row, int col, char[][]board){
-        if(row<0 || row>= n || col <0 || col>= m || board[row][col] == '.'){
+    
+    public void Dfs(int row, int col, char[][] board){
+        if(row<0 || col<0 || row >= m || col >= n || board[row][col] == '.'){
             return;
         }
         
-        board[row][col]= '.';
-        
-        Dfs(row + 1, col, board);
-        Dfs(row - 1, col, board);
+        board[row][col] = '.';
+        Dfs(row+1, col, board);
+        Dfs(row-1, col, board);
         Dfs(row, col+1, board);
         Dfs(row, col-1, board);
     }
