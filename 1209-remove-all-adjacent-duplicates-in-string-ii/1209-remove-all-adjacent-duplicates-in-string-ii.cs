e2c6ab69fc8d634499a344c7f791c9a34aae0d1d
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-09-02 11:41:10 +0300

    Time: 82 ms (70.29%), Space: 43.1 MB (47.83%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cs
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cs
@@ -5,13 +5,13 @@ public class Solution {
         foreach(var c in s){
             if(!stack.Any() || stack.Peek().ch != c){
                 stack.Push((c, 1));
-            }else {
+            }else{
                 var (ch, count) = stack.Pop();
                 if(count + 1 != k)
                     stack.Push((ch, count + 1));
-            }
-            
+            } 
         }
+        
         
         return string.Concat(stack.Select(i=>new string(i.ch, i.count)).Reverse());
     }
