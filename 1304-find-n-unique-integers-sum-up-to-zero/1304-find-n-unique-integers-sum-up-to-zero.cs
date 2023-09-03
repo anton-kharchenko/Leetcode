b92a545cd968f6f6d680eb5d74c72865018b2b50
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-09-03 17:35:28 +0300

    Time: 84 ms (74.31%), Space: 36.8 MB (7.34%) - LeetHub

--- 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.cs
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.cs
@@ -1,14 +1,15 @@
 public class Solution {
     public int[] SumZero(int n) {
         var res = new List<int>();
-        if(n % 2== 1){
+        if(n % 2 == 1){
             res.Add(0);
             n--;
         }
-        for(var i = 1; i<=n/2;i++){
+        for(var i = 1; i<=n/2; i++){
             res.Add(i);
             res.Add(-i);
         }
+        
         return res.ToArray();
     }
 }
