c715445a60965454cc74630f5f99deca72899c72
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-09-05 14:54:46 +0300

    Time: 131 ms (98.98%), Space: 47.2 MB (90.86%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cs
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cs
@@ -1,38 +1,43 @@
 public class Solution {
     public int[] FindOrder(int numCourses, int[][] prerequisites) {
-        var res = new List<int>();
         var adj = new List<List<int>>();
-        
+        var res = new List<int>();
         for(var i = 0; i<numCourses; i++){
             adj.Add(new List<int>());
         }
-        var indegree = new int[numCourses];
         
-        for(var i = 0; i< prerequisites.Length; i++){
-            adj[prerequisites[i][1]].Add(prerequisites[i][0]);
-            indegree[prerequisites[i][0]]++;
+        var indeegree = new int[numCourses];
+        
+        foreach(var prerequisite in prerequisites){
+            adj[prerequisite[1]].Add(prerequisite[0]);
+            indeegree[prerequisite[0]]++;
         }
         
         var q = new Queue<int>();
         
-        for(var i = 0; i<indegree.Length; i++){
-            if(indegree[i] == 0)
+        for(var i = 0; i<indeegree.Length; i++){
+            if(indeegree[i] == 0){
                 q.Enqueue(i);
+            }
         }
         
-        while(q.Count>0){
+        
+        // BFS
+        while(q.Count> 0){
             var curr = q.Dequeue();
             res.Add(curr);
-            foreach(var next in adj[curr]){
-                indegree[next]--;
-                if(indegree[next] == 0)
-                    q.Enqueue(next);
-            }
             
+            foreach(var next in adj[curr]){
+                indeegree[next]--;
+                if(indeegree[next] == 0){
+                    q.Enqueue(next);
+                }
+            }
         }
         
-        if(numCourses> res.Count) return new int[0];
         
+        if(res.Count< numCourses)
+            return new int[0];
         
         return res.ToArray();
     }
