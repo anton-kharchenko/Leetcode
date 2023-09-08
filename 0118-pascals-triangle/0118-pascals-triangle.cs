3d6451117d0e766fad1f39663aa7a72ef95873dc
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-09-08 12:58:03 +0300

    Time: 107 ms (5.58%), Space: 37.1 MB (43.97%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cs
+++ 0118-pascals-triangle/0118-pascals-triangle.cs
@@ -3,11 +3,11 @@ public class Solution {
         var ans = new List<IList<int>>();
         ans.Add(new List<int>(){1});
         
+        
         for(var i = 1; i<numRows; i++){
             var temp = new List<int>();
-            
-            for(var j = 0; j<=i; j++){
-                if(j==0 || j== i){
+            for(var j = 0; j<= i; j++){
+                if(j==0 || j==i){
                     temp.Add(1);
                 }else{
                     temp.Add(ans[i-1][j-1] + ans[i-1][j]);
