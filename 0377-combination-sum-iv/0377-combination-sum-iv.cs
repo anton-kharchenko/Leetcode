8c0f9abae1ac1e6ea4fefcd58ca1ca38cb4b418a
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-09-09 12:32:10 +0300

    Time: 80 ms (57.47%), Space: 38.3 MB (50.57%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cs
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cs
@@ -1,17 +1,16 @@
 public class Solution {
     public int CombinationSum4(int[] nums, int target) {
-      var comb = new int[target + 1];
-      comb[0] = 1;
+        var dp = new int[target+1];
+        dp[0] = 1;
         
-        for(var i = 1; i<=target; i++){
-            for(var j = 0; j<nums.Length;j++ ){
-                if(i-nums[j]>= 0){
-                    comb[i] += comb[i - nums[j]];
+        for(var i = 1; i<=target;i++){
+            foreach(var num in nums){
+                if(i-num>= 0){
+                    dp[i] += dp[i-num];
                 }
             }
         }
         
-        
-      return comb[target];  
-    }
+        return dp[target];
+     }
 }
