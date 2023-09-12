fdd4e68a73fc9d20edfa62128df1238866e5a3c4
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-09-12 19:07:00 +0300

    Time: 107 ms (26.39%), Space: 45.6 MB (6.94%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cs
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cs
@@ -1,15 +1,17 @@
 public class Solution {
     public int MinDeletions(string s) {
-       int deleted = 0;
-       var hs = new HashSet<int>(); 
-       foreach(var group in s.GroupBy(i=>i)){
-           var count = group.Count();
-           while(count>0 && hs.Add(count) == false){
-               deleted++;
-               count--;
-           }
-       } 
+        var deleted = 0; // 2
+        var hs = new HashSet<int>(); // [3, 2,  1]
+        // aaabbbcc
+        foreach(var group in s.GroupBy(i=>i)){
+            var count = group.Count(); // cc - 2
+            while(count > 0 && hs.Add(count) == false){
+                deleted++;
+                count--;
+            }
+            
+        }
         
-       return deleted; 
+        return deleted; // 2
     }
 }
