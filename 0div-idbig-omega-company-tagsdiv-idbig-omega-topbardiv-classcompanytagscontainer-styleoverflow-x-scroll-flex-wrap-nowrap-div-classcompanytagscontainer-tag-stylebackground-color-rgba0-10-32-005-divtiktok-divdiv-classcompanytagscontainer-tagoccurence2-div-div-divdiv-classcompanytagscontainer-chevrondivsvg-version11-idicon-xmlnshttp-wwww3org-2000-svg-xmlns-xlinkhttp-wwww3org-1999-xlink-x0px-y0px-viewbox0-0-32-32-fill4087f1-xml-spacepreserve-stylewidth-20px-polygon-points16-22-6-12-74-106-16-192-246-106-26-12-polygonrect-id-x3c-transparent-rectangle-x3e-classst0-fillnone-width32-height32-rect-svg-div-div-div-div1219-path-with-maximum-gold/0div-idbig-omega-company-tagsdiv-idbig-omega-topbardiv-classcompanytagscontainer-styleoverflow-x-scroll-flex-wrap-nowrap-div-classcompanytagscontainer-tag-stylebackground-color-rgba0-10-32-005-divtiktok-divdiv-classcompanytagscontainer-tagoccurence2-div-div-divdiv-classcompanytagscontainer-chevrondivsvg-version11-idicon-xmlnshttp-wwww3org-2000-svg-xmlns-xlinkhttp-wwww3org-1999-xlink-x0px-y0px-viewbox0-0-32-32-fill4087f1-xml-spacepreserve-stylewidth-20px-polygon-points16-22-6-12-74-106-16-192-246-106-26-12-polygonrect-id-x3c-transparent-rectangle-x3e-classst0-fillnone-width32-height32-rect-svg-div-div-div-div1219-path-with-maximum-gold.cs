1d666e652abdb9d8b633b846503c816d1ab6c22b
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-09-20 15:27:02 +0300

    Time: 167 ms (70.27%), Space: 38.4 MB (48.65%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divtiktok-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1219-path-with-maximum-gold/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divtiktok-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1219-path-with-maximum-gold.cs
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divtiktok-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1219-path-with-maximum-gold/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divtiktok-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1219-path-with-maximum-gold.cs
@@ -1,37 +1,40 @@
 public class Solution {
     int n = 0;
     int m = 0;
-    List<int> result = new List<int>();
+    List<int> results = new List<int>();
     public int GetMaximumGold(int[][] grid) {
         n = grid.Length;
         m = grid[0].Length;
         
-        for(var i = 0; i<grid.Length; i++){
-            for(var j = 0; j<grid[0].Length; j++){
+        for(var i = 0; i< n; i++){
+            for(var j = 0; j<m; j++){
                 if(grid[i][j] != 0){
-                   result.Add(Dfs(i, j, grid, 0));
+                    results.Add(Dfs(i, j, grid, 0));
                 }
             }
         }
-        if(!result.Any()) return 0;
         
-        return result.Max();
+        if(!results.Any()) return 0;
+        return results.Max();
     }
     
-    public int Dfs(int i, int j,  int[][] grid, int sum){
-        if(i>= n || j>= m || i<0 || j<0 || grid[i][j] == 0){
+    public int Dfs(int i, int j, int[][] grid, int sum){
+        if(i <0 || j< 0 || i>= n || j>=m || grid[i][j] == 0){
             return sum;
         }
         
-        sum+= grid[i][j];
+        sum += grid[i][j];
         var temp = grid[i][j];
         grid[i][j] = 0;
-        var top = Dfs(i+1,j, grid, sum);
-        var bottom = Dfs(i-1, j, grid, sum);
+        
         var left = Dfs(i, j-1, grid, sum);
         var right = Dfs(i, j+1, grid, sum);
+        var top = Dfs(i+1, j, grid, sum);
+        var bottom = Dfs(i-1, j, grid, sum);
+        
         grid[i][j] = temp;
         
-       return Math.Max(Math.Max(top, bottom), Math.Max(left, right));
+        return Math.Max(Math.Max(left, right), Math.Max(top, bottom));
     }
+    
 }
