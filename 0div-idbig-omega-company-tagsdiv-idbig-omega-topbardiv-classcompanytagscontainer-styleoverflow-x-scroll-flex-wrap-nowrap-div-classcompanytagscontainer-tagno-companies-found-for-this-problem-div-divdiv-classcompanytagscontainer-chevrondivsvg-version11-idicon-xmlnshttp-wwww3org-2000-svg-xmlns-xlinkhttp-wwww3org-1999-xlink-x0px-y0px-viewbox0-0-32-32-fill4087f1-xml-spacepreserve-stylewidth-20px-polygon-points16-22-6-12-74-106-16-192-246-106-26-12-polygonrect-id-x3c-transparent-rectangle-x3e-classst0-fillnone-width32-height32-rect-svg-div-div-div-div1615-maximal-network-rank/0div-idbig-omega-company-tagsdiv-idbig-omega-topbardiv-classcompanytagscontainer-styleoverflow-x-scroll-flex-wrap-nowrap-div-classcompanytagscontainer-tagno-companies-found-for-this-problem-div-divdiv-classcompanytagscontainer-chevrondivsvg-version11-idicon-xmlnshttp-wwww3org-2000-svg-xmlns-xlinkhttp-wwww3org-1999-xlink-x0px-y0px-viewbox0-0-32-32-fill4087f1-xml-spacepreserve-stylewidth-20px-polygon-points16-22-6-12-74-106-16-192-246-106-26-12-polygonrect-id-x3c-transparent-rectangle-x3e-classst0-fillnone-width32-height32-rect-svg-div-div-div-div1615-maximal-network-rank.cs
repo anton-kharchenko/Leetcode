2844ea1e7cec2c85b42d85d36892f36086ec546f
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-09-23 11:42:52 +0300

    Time: 155 ms (36.14%), Space: 60.5 MB (30.99%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1615-maximal-network-rank/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1615-maximal-network-rank.cs
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1615-maximal-network-rank/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1615-maximal-network-rank.cs
@@ -1,33 +1,32 @@
 public class Solution {
     public int MaximalNetworkRank(int n, int[][] roads) {
         var graph = BuildGraph(n, roads);
-        var maxRang = 0;
-        
+        var max = 0;
         for(var i = 0; i<n; i++){
             for(var j = i+1; j<n;j++){
                 if(!graph[i].Contains(j)){
-                    maxRang = Math.Max(maxRang, graph[i].Count + graph[j].Count);
+                   max = Math.Max(max, graph[i].Count + graph[j].Count); 
                 }else{
-                    maxRang = Math.Max(maxRang, (graph[i].Count + graph[j].Count)-1);
+                    max = Math.Max(max, (graph[i].Count + graph[j].Count) - 1 );
                 }
             }
         }
-        
-        return maxRang;
+        return max;
     }
     
     public Dictionary<int, HashSet<int>> BuildGraph(int n, int[][] roads){
-        var dict = new Dictionary<int, HashSet<int>>();
+        var graph = new Dictionary<int, HashSet<int>>();
         
         for(var i = 0; i<n; i++){
-           dict.Add(i, new HashSet<int>()); 
+            graph.Add(i, new HashSet<int>());
         }
         
-        foreach(var road in roads){
-            dict[road[0]].Add(road[1]);
-            dict[road[1]].Add(road[0]);
+        for(var i = 0; i<roads.Length; i++){
+            var road = roads[i];
+            graph[road[0]].Add(road[1]);
+            graph[road[1]].Add(road[0]);
         }
         
-        return dict;
+        return graph;
     }
 }
