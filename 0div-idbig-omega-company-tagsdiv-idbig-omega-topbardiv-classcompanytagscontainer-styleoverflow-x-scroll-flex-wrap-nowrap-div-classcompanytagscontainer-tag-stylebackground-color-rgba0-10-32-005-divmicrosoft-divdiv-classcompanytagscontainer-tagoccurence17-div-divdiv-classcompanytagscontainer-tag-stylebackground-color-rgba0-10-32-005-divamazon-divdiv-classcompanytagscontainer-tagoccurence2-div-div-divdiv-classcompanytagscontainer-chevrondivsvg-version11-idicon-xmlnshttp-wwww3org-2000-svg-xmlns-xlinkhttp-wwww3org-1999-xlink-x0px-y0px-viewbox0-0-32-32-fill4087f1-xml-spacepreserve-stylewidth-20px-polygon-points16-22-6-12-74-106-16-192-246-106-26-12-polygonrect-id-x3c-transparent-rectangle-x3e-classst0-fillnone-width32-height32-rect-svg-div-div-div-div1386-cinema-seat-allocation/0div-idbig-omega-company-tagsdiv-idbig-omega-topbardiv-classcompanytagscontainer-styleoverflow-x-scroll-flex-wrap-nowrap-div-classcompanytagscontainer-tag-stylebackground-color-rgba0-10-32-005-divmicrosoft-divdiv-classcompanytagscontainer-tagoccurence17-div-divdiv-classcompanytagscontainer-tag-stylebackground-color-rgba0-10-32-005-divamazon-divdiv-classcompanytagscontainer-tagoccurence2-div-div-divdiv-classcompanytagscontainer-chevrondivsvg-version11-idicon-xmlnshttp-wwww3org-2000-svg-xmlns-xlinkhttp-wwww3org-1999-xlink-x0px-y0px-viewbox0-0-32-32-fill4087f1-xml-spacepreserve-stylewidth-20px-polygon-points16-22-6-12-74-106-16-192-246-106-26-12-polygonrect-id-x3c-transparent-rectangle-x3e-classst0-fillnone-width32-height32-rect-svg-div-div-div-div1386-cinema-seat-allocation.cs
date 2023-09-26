cffc6232486ee47f95e4ab86308a740e5c0a64ec
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-09-26 17:04:21 +0300

    Time: 221 ms (55.26%), Space: 69.8 MB (23.68%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence17-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1386-cinema-seat-allocation/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence17-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1386-cinema-seat-allocation.cs
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence17-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1386-cinema-seat-allocation/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence17-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1386-cinema-seat-allocation.cs
@@ -1,39 +1,42 @@
 public class Solution {
     public int MaxNumberOfFamilies(int n, int[][] reservedSeats) {
-        var hs = new Dictionary<int, HashSet<int>>();
+        var dict = new Dictionary<int, HashSet<int>>();
+        
         foreach(var reservedSeat in reservedSeats){
-            hs.TryAdd(reservedSeat[0], new HashSet<int>());
-            hs[reservedSeat[0]].Add(reservedSeat[1]);
+            var row = reservedSeat[0];
+            var col = reservedSeat[1];
+            dict.TryAdd(row, new HashSet<int>());
+            dict[row].Add(col);    
         }
-        int res = (n - hs.Count) * 2;
-        foreach(var col in hs.Values){
-            var reservedMiddle = false; 
-            if(!IsReserved(col, 2, 5)){
-                reservedMiddle = true;
+        var res = (n - dict.Count) * 2;
+        
+        foreach(var hs in dict.Values){
+            var reservedMid = false;
+            if(!IsReserved(2, 5, hs)){
                 res++;
+                reservedMid = true;
             }
             
-             if(!IsReserved(col, 6, 9)){
-                reservedMiddle = true;
+            if(!IsReserved(6, 9, hs)){
                 res++;
+                reservedMid = true;
             }
             
-            
-            if(!reservedMiddle && !IsReserved(col, 4, 7)){
+            if(!IsReserved(4, 7, hs) && !reservedMid){
                 res++;
             }
-        }
+        }   
         
         return res;
     }
     
-    public bool IsReserved(HashSet<int> col, int low, int high){
+    public bool IsReserved(int low, int high, HashSet<int> hs){
         for(var i = low; i<= high; i++){
-            if(col.Contains(i)){
+            if(hs.Contains(i)) 
                 return true;
-            }
         }
-        
         return false;
     }
+    
+    
 }
