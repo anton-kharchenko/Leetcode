45d4d2ec189e4ae541f4be1bdaa6aec6a9b95941
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-10-09 14:56:02 +0300

    Time: 126 ms (91.49%), Space: 45 MB (45.00%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array.cs
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array.cs
@@ -1,38 +1,35 @@
 public class Solution {
     public int[] SearchRange(int[] nums, int target) {
-       var searchRange = new int[2]{-1, -1};
-       if(nums.Length == 0)
+        var searchRange = new int[2]{-1, -1};
+        if(nums.Length == 0)
             return searchRange;
-       searchRange[0] = FindIndex(nums, target, true);
-       searchRange[1] = FindIndex(nums, target, false); 
-       return searchRange; 
-    }
-    
-    public int FindIndex(int[] nums, int target, bool isStart){
-        int start = 0; 
-        int end = nums.Length - 1; 
-        int result = -1;
         
-        while(start <= end)
-        {
-            int mid = start + (end - start) / 2;
-            
-            if(nums[mid] == target)
-            {
+        searchRange[0] =  FindIndex(nums, target, true);
+        searchRange[1] =  FindIndex(nums, target, false);
+        return searchRange;
+    }
+
+    public int FindIndex(int[] nums, int target, bool IsStart){
+        var result = -1;
+        var start = 0;
+        var end = nums.Length - 1;
+        
+        while(start<=end){
+            var mid = start + (end - start) / 2;
+            if(nums[mid] == target){
                 result = mid;
-                if(isStart){
+                if(IsStart){
                     end = mid - 1;
                 }else{
                     start = mid + 1;
                 }
-            }
-            else if(nums[mid] < target)
+            }else if(nums[mid]< target){
                 start = mid + 1;
-            else
+            }else{
                 end = mid - 1;
+            }
         }
         
         return result;
     }
-    
 }
