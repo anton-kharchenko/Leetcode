5c9615411f0b0afa89715b6da937ca273031d2b5
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-10-16 01:51:11 +0300

    Time: 67 ms (79.57%), Space: 40.4 MB (93.35%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divdoordash-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divkarat-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1143-longest-common-subsequence/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divdoordash-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divkarat-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1143-longest-common-subsequence.cs
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divdoordash-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divkarat-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1143-longest-common-subsequence/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divdoordash-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divkarat-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1143-longest-common-subsequence.cs
@@ -1,49 +1,55 @@
 public class Solution {
     
     public int LongestCommonSubsequence(string text1, string text2) {
-        // return Dfs(text1, text2, 0, 0);
-        var length1 = text1.Length;
-        var length2 = text2.Length;
-        var dp = new int[length1 + 1, length2 + 1];
-        
-        for(var i = 0; i<length1; i++){
-            for(var j = 0; j<length2; j++){
-              if(text1[i]==text2[j]){
-                  dp[i+1, j+1] =  1 + dp[i,j];
-              }else{
-                  dp[i+1, j+1] = Math.Max(dp[i+1, j], dp[i, j+1]);
-            }
-          }
-        }
-        return dp[length1, length2];
+        // return Recursive(text1, text2, 0, 0);
+        // return RecursiveWithCache(text1, text2, 0, 0, new int[text1.Length,text2.Length]);
+        return DP(text1, text2, 0, 0, new int[text1.Length + 1, text2.Length + 1]);
+     }
+     
+    
+     public int Recursive(string s1, string s2, int index1, int index2){
+        if(index1 == s1.Length || index2 == s2.Length)
+            return 0;
+       if(s1[index1]==s2[index2]){
+           return 1+ Recursive(s1, s2, index1+1, index2 + 1);
+         }else{
+            return Math.Max(Recursive(s1, s2, index1, index2 + 1), 
+                           Recursive(s1, s2, index1+1, index2));
       }
-}
-//     1.Recursive solution 
-//     public int Dfs(string s1, string s2, int index1, int index2){
-//         if(index1 == s1.Length || index2 == s2.Length)
-//             return 0;
-//         if(s1[index1]==s2[index2]){
-//             return 1+ Dfs(s1, s2, index1+1, index2 + 1);
-//         }else{
-//             return Math.Max(Dfs(s1, s2, index1, index2 + 1), 
-//                             Dfs(s1, s2, index1+1, index2));
-//         }
-//     }
-
-//    2. Recursive solution with cache 
-//         public int Dfs(string s1, string s2, int index1, int index2, int[,] cache){
-//         if(index1 == s1.Length || index2 == s2.Length)
-//             return 0;
-//         if(cache[index1, index2] != 0)
-//             return cache[index1, index2];
+    }
+    
+    public int RecursiveWithCache(string s1, string s2, int index1, int index2, int[,] cache){
+         if(index1 == s1.Length || index2 == s2.Length)
+             return 0;
+        if(cache[index1, index2] != 0)
+             return cache[index1, index2];
            
-//         if(s1[index1]==s2[index2]){
-//             cache[index1, index2] = 1+ Dfs(s1, s2, index1+1, index2 + 1, cache);
-//         }else{
-//             cache[index1, index2] = Math.Max(Dfs(s1, s2, index1, index2 + 1, cache), 
-//                                              Dfs(s1, s2, index1+1, index2, cache));
-//         }
-//         return cache[index1, index2];
-//     }
-// }
+         if(s1[index1]==s2[index2]){
+            cache[index1, index2] = 1 + RecursiveWithCache(s1, s2, index1+1, index2 + 1, cache);
+        }else{
+             cache[index1, index2] = Math.Max(RecursiveWithCache(s1, s2, index1, index2 + 1, cache), 
+                                             RecursiveWithCache(s1, s2, index1+1, index2, cache));
+        }
+
+         return cache[index1, index2];
+    }
+    
+    public int DP(string s1, string s2, int index1, int index2, int[,] dp){
+        var s1Length = s1.Length;
+        var s2Length = s2.Length;
+        
+        for(var i = 0; i<s1Length; i++){
+            for(var j = 0; j<s2Length; j++){
+                if(s1[i] == s2[j]){
+                    dp[i+1, j+1] = 1  + dp[i, j];
+                }else{
+                    dp[i+1, j+1] = Math.Max(dp[i+1, j], dp[i, j+1]);
+                }
+            }
+        }
+        
+        return dp[s1Length, s2Length];
+        
+    }
+}
 
