bc3be6106bca0f0128255ce9a85c9e78cc9d5638
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-11-24 20:36:26 +0200

    Time: 68 ms (48.41%), Space: 38.1 MB (49.68%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cs
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cs
@@ -1,33 +1,32 @@
 public class Solution {
     public int CharacterReplacement(string s, int k) {
         Dictionary<char, int> dictionary = new Dictionary<char, int>();
-        int result = 0;
-        int max = 0;
-        int start = 0;
-        int end = 0;
+        int windowStart = 0;
+        int windowEnd = 0;
+        int maxLetterCount = 0;
+        int maxSubstringCount = 0;
         
-        while(end<s.Length){
-            char letter = s[end];
-            
+        while(windowEnd<s.Length){
+            char letter = s[windowEnd];
             if(dictionary.ContainsKey(letter)){
                 dictionary[letter] += 1;
             }else{
                 dictionary.Add(letter, 1);
             }
-            end += 1;
             
-            max = Math.Max(max, dictionary[letter]);
+            windowEnd += 1;
             
-            if(end - start - max > k ){
-                char letterToDelete = s[start];
+            maxLetterCount = Math.Max(maxLetterCount, dictionary[letter]);
+            
+            if(windowEnd - windowStart - maxLetterCount > k ){
+                char letterToDelete = s[windowStart];
                 dictionary[letterToDelete] -= 1;
-                start += 1;
+                windowStart += 1;
             }
             
-            result = Math.Max(result, end - start);
+            maxSubstringCount = Math.Max(maxSubstringCount, windowEnd - windowStart);
         }
         
-        
-        return result;
+        return maxSubstringCount;
     }
 }
