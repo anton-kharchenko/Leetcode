a6412f43db5439071aab3679c68ebba2868cb8b1
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-11-27 19:51:45 +0200

    Time: 101 ms (24.49%), Space: 41 MB (85.71%) - LeetHub

--- 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.cs
+++ 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.cs
@@ -2,33 +2,31 @@ public class Solution {
     public int LengthOfLongestSubstringTwoDistinct(string s) {
         int windowStart = 0;
         int windowEnd = 0;
-        int winodwSize = 0;
-        int longestSubstringLength = 0;
-        Dictionary<char, int> uniqueLetters = new Dictionary<char, int>();
-        
-        while(windowEnd<s.Length){
+        int windowSize = 0;
+        int longestSubstingLength = 0;
+        Dictionary<char, int> dictionary = new Dictionary<char, int>();
+        while(windowEnd < s.Length){
             char letter = s[windowEnd];
-            if (!uniqueLetters.ContainsKey(letter)) {
-                uniqueLetters[letter] = 1;
-            } else {
-                uniqueLetters[letter] += 1;
+            if(dictionary.ContainsKey(letter)){
+                dictionary[letter] += 1;
+            }else{
+                dictionary.Add(letter, 1);
             }
             windowEnd += 1;
-             
-            if(uniqueLetters.Count > 2){
+            
+            if(dictionary.Count > 2){
                 char letterToRemove = s[windowStart];
-                uniqueLetters[letterToRemove] -= 1;
-                if(uniqueLetters[letterToRemove] == 0){
-                   uniqueLetters.Remove(letterToRemove); 
+                dictionary[letterToRemove] -= 1;
+                if(dictionary[letterToRemove]<=0){
+                    dictionary.Remove(letterToRemove);
                 }
                 windowStart += 1;
             }
             
-            winodwSize = windowEnd - windowStart;
-            longestSubstringLength = Math.Max(longestSubstringLength, winodwSize);
-
+            windowSize =  windowEnd - windowStart;
+            longestSubstingLength = Math.Max(longestSubstingLength, windowSize);
         }
         
-        return longestSubstringLength;
+        return longestSubstingLength;
     }
 }
