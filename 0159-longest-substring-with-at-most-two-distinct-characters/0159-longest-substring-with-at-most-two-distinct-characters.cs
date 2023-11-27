db866a3124765ede5d4f5c632c72efdb0661b059
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-11-27 20:31:28 +0200

    Time: 96 ms (44.90%), Space: 41.3 MB (46.94%) - LeetHub

--- 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.cs
+++ 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.cs
@@ -2,31 +2,33 @@ public class Solution {
     public int LengthOfLongestSubstringTwoDistinct(string s) {
         int windowStart = 0;
         int windowEnd = 0;
-        int windowSize = 0;
-        int longestSubstingLength = 0;
+        int windowSize  = 0;
+        int result = 0;
         Dictionary<char, int> dictionary = new Dictionary<char, int>();
-        while(windowEnd < s.Length){
+        
+        while(windowEnd<s.Length){
             char letter = s[windowEnd];
             if(dictionary.ContainsKey(letter)){
                 dictionary[letter] += 1;
             }else{
                 dictionary.Add(letter, 1);
             }
+            
             windowEnd += 1;
             
-            if(dictionary.Count > 2){
-                char letterToRemove = s[windowStart];
-                dictionary[letterToRemove] -= 1;
-                if(dictionary[letterToRemove]<=0){
-                    dictionary.Remove(letterToRemove);
+            if(dictionary.Count>2){
+                char letterToDelete = s[windowStart];
+                dictionary[letterToDelete] -= 1;
+                if(dictionary[letterToDelete] == 0){
+                    dictionary.Remove(letterToDelete);
                 }
                 windowStart += 1;
             }
             
-            windowSize =  windowEnd - windowStart;
-            longestSubstingLength = Math.Max(longestSubstingLength, windowSize);
+            windowSize = windowEnd - windowStart;
+            result = Math.Max(result, windowSize);
         }
         
-        return longestSubstingLength;
+        return result;
     }
 }
