338b66bb3b2077695585dab00483b3295b29250f
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-11-27 20:57:53 +0200

    Time: 96 ms (44.90%), Space: 41.4 MB (46.94%) - LeetHub

--- 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.cs
+++ 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.cs
@@ -2,21 +2,20 @@ public class Solution {
     public int LengthOfLongestSubstringTwoDistinct(string s) {
         int windowStart = 0;
         int windowEnd = 0;
-        int windowSize  = 0;
-        int result = 0;
+        int windowSize = 0;
+        int longestSubstringLength = 0;
         Dictionary<char, int> dictionary = new Dictionary<char, int>();
         
-        while(windowEnd<s.Length){
+        while(windowEnd<s.Length){ // O(N), O(1)
             char letter = s[windowEnd];
             if(dictionary.ContainsKey(letter)){
                 dictionary[letter] += 1;
             }else{
                 dictionary.Add(letter, 1);
             }
-            
             windowEnd += 1;
             
-            if(dictionary.Count>2){
+            if(dictionary.Count > 2){
                 char letterToDelete = s[windowStart];
                 dictionary[letterToDelete] -= 1;
                 if(dictionary[letterToDelete] == 0){
@@ -25,10 +24,10 @@ public class Solution {
                 windowStart += 1;
             }
             
-            windowSize = windowEnd - windowStart;
-            result = Math.Max(result, windowSize);
+            windowSize =  windowEnd - windowStart;
+            longestSubstringLength = Math.Max(longestSubstringLength, windowSize);
         }
         
-        return result;
+        return longestSubstringLength;
     }
 }
