af5f4083ae8d36f180a6e5b9e0bcb64edb3e13eb
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-11-28 21:23:11 +0200

    Time: 133 ms (51.82%), Space: 42.7 MB (90.24%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cs
+++ 0048-rotate-image/0048-rotate-image.cs
@@ -1,20 +1,19 @@
 public class Solution {
     public void Rotate(int[][] matrix) {
         int length = matrix.Length;
-        
-        for(int layer = 0; layer < length/2; layer++){
-           int first  =  layer;
-           int last =  length - 1 - layer;
+        for(int level = 0; level<length /2; level++){
+            int first = level;
+            int last = length - 1 - level;
             
-           for(int i = first; i<last;i++ ){
-               int offset = i - first;
-               
-               int topElemnt = matrix[first][i];
-               matrix[first][i] = matrix[last - offset][first];
-               matrix[last - offset][first] = matrix[last][last - offset];
-               matrix[last][last - offset] = matrix[i][last];
-               matrix[i][last] = topElemnt;
-           } 
+            for(int i = first; i<last; i++){
+                int offset = i - first;
+                
+                int topLeftElement = matrix[first][i];
+                matrix[first][i] = matrix[last - offset][first]; // top left
+                matrix[last - offset][first] = matrix[last][last-offset]; // bottom left
+                matrix[last][last-offset] = matrix[i][last]; // bottom right
+                matrix[i][last] = topLeftElement; // top right
+            }
         }
     }
 }
