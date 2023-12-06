77d2f797989283e755911d18ed1da88f51e1ed98
Anton Kharchenko <55918247+antonKharchenko1997@users.noreply.github.com>
2023-12-06 08:43:08 +0200

    Time: 24 ms (37.84%), Space: 26.6 MB (70.27%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cs
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cs
@@ -1,17 +1,19 @@
 public class Solution {
     public int TotalMoney(int n) {
-        int coefficient = 1;
-        int money = 1;
+        int cof = 1;
         int totalMoney = 0;
-        for (int i = 1; i <=n; i++)
-        {
-            if(i / 7 > 0 &&  i % 7 == 1){
-                coefficient += 1;
-                money = coefficient;
+        int money = 1;
+        
+        for(int i = 1; i<=n; i++ ){
+            if(i/7>0 && i%7==1 ){
+                cof += 1;
+                money = cof;
             }
+            
             totalMoney += money;
             money += 1;
         }
-        return  totalMoney; 
+        
+        return totalMoney;
     }
 }
