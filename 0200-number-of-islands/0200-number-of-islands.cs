98a5c55a5aec893b52073b8e641dbdf2c65fac8d
Anton Kharchenko <55918247+anton-kharchenko@users.noreply.github.com>
2024-05-22 16:02:42 +0300

    Time: 123 ms (70.4%), Space: 51.4 MB (72.87%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cs
+++ 0200-number-of-islands/0200-number-of-islands.cs
@@ -1,29 +1,29 @@
 public class Solution {
- public int NumIslands(char[][] grid) {
-     var n = grid.Length;
-     var m = grid[0].Length;
-     var res = 0;
-    for(var i = 0; i<n; i++){
-        for(var j = 0; j<m; j++){
-            if(grid[i][j] == '1'){
-                res++;
-                Dfs(i, j, n, m, grid);
+    public int NumIslands(char[][] grid) {
+        int rows = grid.Length;
+        int cols = grid[0].Length;
+        int result = 0;
+        for(int i = 0; i<rows; i++){
+            for(int j = 0; j< cols; j++){
+                if(grid[i][j] == '1'){
+                    RemoveIsland(grid, i, j, rows, cols);
+                    result += 1;
+                }
             }
         }
+        return result;
     }
-     
-     return res;
-  }
-    
-    public void Dfs(int i, int j, int n, int m, char[][] grid){
-        if(i<0 || j<0|| i>=n||j>=m || grid[i][j] == '0'){
+
+
+    public void RemoveIsland(char[][] grid, int row, int col, int rows, int cols){
+        if(row<0 || col<0 || row>= rows || col>= cols || grid[row][col] == '0'){
             return;
         }
-        
-        grid[i][j] = '0';
-        Dfs(i+1, j, n, m, grid);
-        Dfs(i-1, j, n, m, grid);
-        Dfs(i, j+1, n, m, grid);
-        Dfs(i, j-1, n, m, grid);
+        grid[row][col] = '0';
+        RemoveIsland(grid, row + 1, col, rows, cols);
+        RemoveIsland(grid, row - 1, col, rows, cols);
+        RemoveIsland(grid, row, col + 1, rows, cols);
+        RemoveIsland(grid, row, col - 1, rows, cols);
     }
+
 }
