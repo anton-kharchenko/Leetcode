d33886bab7a747752518677635d02dae79bba0c1
Anton Kharchenko <55918247+anton-kharchenko@users.noreply.github.com>
2024-06-09 19:46:25 +0300

    Time: 31 ms (50.54%), Space: 28.4 MB (98.92%) - LeetHub

--- 1086-divisor-game/1086-divisor-game.cs
+++ 1086-divisor-game/1086-divisor-game.cs
@@ -1,9 +1,13 @@
 public class Solution {
     public bool DivisorGame(int n) {
-        return AliceWon(n, true);
+        return AliceWon(n, true, new int[n + 1]);
     }
 
-    public bool AliceWon(int n, bool isAliceTurn){
+    public bool AliceWon(int n, bool isAliceTurn, int[] cache){
+        if(cache[n] != 0){
+            return cache[n] == 1 ? true : false;
+        }
+
         if((n % 2 == 1 && isAliceTurn) || (n % 2 == 0 && !isAliceTurn) ){
             return false;
         }
@@ -12,6 +16,12 @@ public class Solution {
             return true;
         }
 
-        return AliceWon(n - 1, !isAliceTurn) && AliceWon(n - 2, !isAliceTurn);
+        var res = AliceWon(n - 1, !isAliceTurn, cache) && AliceWon(n - 2, !isAliceTurn, cache);
+        if(res == true){
+            cache[n] = 1;
+        }else{
+            cache[n] = -1;
+        }
+        return res;
     }
 }
