0014c2e6a5f2dcd7cf061e341409270612453961
Anton Kharchenko <55918247+anton-kharchenko@users.noreply.github.com>
2024-06-10 22:33:18 +0300

    Time: 75 ms (86.39%), Space: 47.1 MB (10.06%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cs
+++ 0740-delete-and-earn/0740-delete-and-earn.cs
@@ -1,17 +1,21 @@
 public class Solution {
-    public int DeleteAndEarn(int[] nums) {
-        int[] count = new int[10001];
-
-        foreach(var num in nums)
-            count[num] += num;
-        
-        int next = 0, previous = 0, current = 0;
-        for(var i = 0; i < 10001; i++)
+    public int DeleteAndEarn(int[] nums)
+    {
+        var arr = new int[10001];
+        foreach (var num in nums)
         {
-            next = Math.Max(previous + count[i], current);
-            previous = current;
-            current = next;
+            arr[num] += num;
         }
-        return next;
+
+        var cache = new int[arr.Length];
+        cache[0] = arr[0];
+        cache[1] = arr[1];
+        for (int i = 2; i < arr.Length; i++)
+        {
+            cache[i] = Math.Max(arr[i] + cache[i - 2],  cache[i - 1]);
+        }
+        
+        return cache[arr.Length - 1];
     }
+
 }
