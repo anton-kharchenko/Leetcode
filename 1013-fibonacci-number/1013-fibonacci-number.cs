4bb1d10dd5387b398af5f53353c10e143e3a1c78
Anton Kharchenko <55918247+anton-kharchenko@users.noreply.github.com>
2024-06-10 08:25:22 +0300

    Time: 26 ms (48.48%), Space: 26.9 MB (28.1%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int Fib(int n) {
+        if(n == 0) return 0;
+        int[] cache = new int[n + 1]; 
+        cache[0] = 0;
+        cache[1] = 1;
+        for(var i = 2; i<= n; i++){
+            cache[i] = cache[i - 1] + cache[i - 2];
+        }
+        return cache[n];
+    }
+}
