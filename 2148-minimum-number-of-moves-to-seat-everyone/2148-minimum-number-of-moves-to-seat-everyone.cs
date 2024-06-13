ad55f6dd2299647f796919af49d91375a02fce20
Anton Kharchenko <55918247+anton-kharchenko@users.noreply.github.com>
2024-06-13 06:53:32 +0300

    Time: 74 ms (60.24%), Space: 42.6 MB (74.7%) - LeetHub

---
+++ 2148-minimum-number-of-moves-to-seat-everyone/2148-minimum-number-of-moves-to-seat-everyone.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int MinMovesToSeat(int[] seats, int[] students) {
+        Array.Sort(seats);
+        Array.Sort(students);
+        var result = 0;
+        for(var i = 0; i<seats.Length; i++){
+            result += Math.Abs(seats[i] - students[i]);
+        }
+        return result;
+    }
+}
