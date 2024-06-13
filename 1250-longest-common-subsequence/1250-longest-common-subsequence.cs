ac7de8ac9ed3c3977c9285a01909807b3c1ca7af
Anton Kharchenko <55918247+anton-kharchenko@users.noreply.github.com>
2024-06-13 21:40:07 +0300

    Time: 63 ms (40.5%), Space: 45.7 MB (40.06%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cs
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cs
@@ -1,19 +1,21 @@
 public class Solution {
     public int LongestCommonSubsequence(string text1, string text2) {
-        var n = text1.Length;
-        var m = text2.Length;
-        var dp = new int[n + 1, m + 1];
-        for(var i = 1; i<=n; i++){
-            for(var j = 1; j<=m; j++){
-                if(text1[i - 1] == text2[j - 1]){
-                    dp[i, j] = 1 + dp[i - 1, j - 1];
+        var length1 = text1.Length;
+        var length2 = text2.Length;
+        var cache = new int[length1 + 1, length2 + 1];
+
+        for(var i = 1; i<= length1; i++){
+            for(var j = 1; j<= length2; j++){
+                if(text1[i-1] == text2[j-1]){
+                  cache[i, j] = 1 + cache[i - 1, j - 1];
                 }else{
-                    dp[i, j] = Math.Max(dp[i-1, j-1], Math.Max(dp[i -1,j], dp[i, j-1]));
+                  cache[i, j] = Math.Max(cache[i - 1, j], cache[i, j - 1]);
                 }
             }
         }
 
-        return dp[n, m];
+        return cache[length1, length2];
     }
+
 }
 
