7ea87ff63bf52657c360c88f8c2cc6b9ff025074
Anton Kharchenko <55918247+anton-kharchenko@users.noreply.github.com>
2024-06-13 22:47:51 +0300

    Time: 61 ms (56.16%), Space: 42.3 MB (29.75%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cs
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cs
@@ -5,19 +5,23 @@ public class Solution
         int sLen = s.Length, pLen = p.Length;
         Dictionary<string,bool> cache = new Dictionary<string,bool>();
         return DFS(0,0);
+
         bool DFS(int i, int j)
         {
-            if(j==pLen) return i==sLen;
+            if(j == pLen) 
+                return i == sLen;
             
             var key = i+","+j;
-            if(cache.ContainsKey(key)) return cache[key];
+
+            if(cache.ContainsKey(key)) 
+                return cache[key];
             
             bool currCharMatch = i<sLen && (s[i]==p[j] || p[j]=='.');
-            if(j+1<pLen && p[j+1]=='*')
-                return cache[key] = DFS(i,j+2) || (currCharMatch && DFS(i+1,j));
 
+            if(j+1 < pLen && p[j+1]=='*')
+                return cache[key] = DFS(i, j+2) || (currCharMatch && DFS(i+1,j));
             else
-                return cache[key] = currCharMatch && DFS(i+1,j+1);
+                return cache[key] = currCharMatch && DFS(i+1, j+1);
         
             return cache[key] = false;
         }
