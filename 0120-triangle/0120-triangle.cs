782a2d4c17c406050dca2cb7bb6ff3e450da83a1
Anton Kharchenko <55918247+anton-kharchenko@users.noreply.github.com>
2024-06-20 07:55:04 +0300

    Time: 87 ms (15.71%), Space: 44 MB (8.23%) - LeetHub

--- 0120-triangle/0120-triangle.cs
+++ 0120-triangle/0120-triangle.cs
@@ -1,9 +1,56 @@
-public class Solution {
-    public int MinimumTotal(IList<IList<int>> triangle) {
-       for (int i = triangle.Count - 1; i > 0; i--)
-            for (int j = 0; j < triangle[i].Count - 1; j++)
-                triangle[i - 1][j] += Math.Min(triangle[i][j], triangle[i][j + 1]);
+public class Solution
+{
+    public int MinimumTotal(IList<IList<int>> triangle)
+    {
+        var lookup = new List<IList<int>>();
+        lookup.Add(new List<int>());
+        lookup[0].Add(triangle[0][0]);
         
-        return triangle[0][0];
+        for (int i = 1; i < triangle.Count; i++)
+        {
+            lookup.Add(new List<int>());
+            
+            for (int j = 0; j < triangle[i].Count; j++)
+            {
+                if (j == 0)
+                {
+                    lookup[i].Add( lookup[i - 1][0] + triangle[i][j]);
+                }
+                else if (j ==  triangle[i].Count - 1)
+                {
+                    lookup[i].Add( lookup[i - 1][^1] + triangle[i][j]);
+                }
+                else
+                {
+                    lookup[i].Add(Math.Min(lookup[i - 1][j - 1], lookup[i - 1][j]) + triangle[i][j]);
+                }
+            }
+        }
+        
+        return lookup.Last().Min();
+       // return Recursion(triangle, 0, 0, new int[triangle.Count, triangle[^1].Count]);
+    }
+
+    public int Recursion(IList<IList<int>> triangle, int index, int finish, int[,] lookup)
+    {
+        if (finish == triangle.Count)
+        {
+            return 0;
+        }
+
+        if (index >= triangle[finish].Count)
+        {
+            return 0;
+        }
+
+        if (lookup[finish, index] != 0)
+        {
+            return lookup[finish, index];
+        }
+
+        lookup[finish, index] = Math.Min(triangle[finish][index] + Recursion(triangle, index + 1, finish + 1, lookup),
+            triangle[finish][index] + Recursion(triangle, index, finish + 1, lookup));
+
+        return lookup[finish, index];
     }
 }
