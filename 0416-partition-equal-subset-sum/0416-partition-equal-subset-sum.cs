b8ee5ecfce5bdc40de3dd6f414df5c53d3d5458f
Anton Kharchenko <55918247+anton-kharchenko@users.noreply.github.com>
2024-06-24 00:07:36 +0300

    Time: 129 ms (43.83%), Space: 50.3 MB (55.25%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cs
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cs
@@ -1,36 +1,27 @@
 public class Solution
 {
-    private bool[,] lookup;
     public bool CanPartition(int[] nums)
     {
-        int sum = 0;
-        foreach (var number in nums)
-            sum += number;
+        var sum = nums.Sum();
         if (sum % 2 != 0) return false;
         sum /= 2;
-        lookup = new bool[nums.Length + 1, sum + 1];
+        var lookup = new bool[nums.Length + 1, sum + 1];
 
-        for (int i = 0; i <= nums.Length; i++)
+        for (var i = 0; i <= nums.Length; i++)
         {
-            for (int j = 0; j <= sum; j++)
+            for (var j = 0; j <= sum; j++)
             {
                 if (i == 0 || j == 0)
-                {
                     lookup[i, j] = false;
-                }else if (nums[i - 1] > j)
-                {
+                else if (nums[i - 1] > j)
                     lookup[i, j] = lookup[i - 1, j];
-                }else if (nums[i - 1] == j)
-                {
+                else if (nums[i - 1] == j)
                     lookup[i, j] = true;
-                }
                 else
-                {
                     lookup[i, j] = lookup[i - 1, j] || lookup[i - 1, j - nums[i - 1]];
-                }
             }
         }
-        
+
         return lookup[nums.Length, sum];
     }
 }
