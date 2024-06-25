1d7526ef2bea65529e9c206718715d4e5c5c6317
Anton Kharchenko <55918247+anton-kharchenko@users.noreply.github.com>
2024-06-25 07:26:25 +0300

    Time: 55 ms (88.64%), Space: 40.7 MB (6.82%) - LeetHub

---
+++ 1114-binary-search-tree-to-greater-sum-tree/1114-binary-search-tree-to-greater-sum-tree.cs
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    int total = 0; 
+    public TreeNode BstToGst(TreeNode root) {
+        Traversal(root);
+        return root;
+    }
+    
+    public void Traversal(TreeNode root){
+        if(root == null){
+            return;
+        }
+        
+        Traversal(root.right);
+        root.val += total;
+        total = root.val;
+        Traversal(root.left);
+    }
+}
