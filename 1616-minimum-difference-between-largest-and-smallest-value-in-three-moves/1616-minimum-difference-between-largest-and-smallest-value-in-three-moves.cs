4d9033ebbf70223d4b2084458e32e31f5404ca81
Anton Kharchenko <55918247+anton-kharchenko@users.noreply.github.com>
2024-07-03 08:15:33 +0300

    Time: 260 ms (9.09%), Space: 58.9 MB (9.09%) - LeetHub

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves.cs
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves.cs
@@ -1,16 +1,34 @@
 public class Solution {
     public int MinDifference(int[] nums) {
         if(nums.Length <= 4) return 0;
-        Array.Sort(nums);
-        int minDiff = Int32.MaxValue;
-        var left = 0;
-        var right = nums.Length - 4;
-        while (left < 4)
-        {
-            minDiff = Math.Min(minDiff, nums[right] - nums[left]);
-            left += 1;
-            right += 1;
+
+        var min_heap = new PriorityQueue<int, int>();
+        var max_heap = new PriorityQueue<int, int>(Comparer<int>.Create((x,y)=>y.CompareTo(x)));
+        foreach(var num in nums){
+            max_heap.Enqueue(num, num);
+            min_heap.Enqueue(num, num);
+            if(min_heap.Count > 4){
+                max_heap.Dequeue();
+                min_heap.Dequeue();
+            }
         }
-        return minDiff;
+
+        var max_array = new int[4];
+        var min_array = new int[4];
+        var index = 0;
+        while(min_heap.Count>0){
+            max_array[index] = max_heap.Dequeue();
+            min_array[index] = min_heap.Dequeue();
+            index += 1;
+        }
+
+        Array.Sort(max_array);
+        Array.Sort(min_array);
+        var result = int.MaxValue;
+        for(var i = 0; i<4; i++){
+            result = Math.Min(result, Math.Abs( max_array[i] - min_array[i]));
+        }
+
+        return result;
     }
 }
