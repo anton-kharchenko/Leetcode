1bbe3afd034f1a7e6dccde8124dc395d37b61c65
Anton Kharchenko <55918247+anton-kharchenko@users.noreply.github.com>
2024-07-15 16:20:54 +0300

    Time: 810 ms (65.76%), Space: 239.2 MB (32.08%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cs
+++ 0146-lru-cache/0146-lru-cache.cs
@@ -1,44 +1,56 @@
 public class LRUCache {
-    Dictionary<int,int> dict=new Dictionary<int,int>();
-    IList<int> list;
-    int cap;
-    
+
+    private readonly int _capacity;
+    private readonly Dictionary<int, LinkedListNode<CacheItem>> _cacheMap;
+    private readonly LinkedList<CacheItem> _cacheList;
+
     public LRUCache(int capacity) {
-        list=new List<int>();
-        cap=capacity;
+        _capacity = capacity;
+        _cacheMap = new Dictionary<int, LinkedListNode<CacheItem>>(capacity);
+        _cacheList = new LinkedList<CacheItem>();
     }
     
     public int Get(int key) {
-        if(!dict.ContainsKey(key))
-            return -1;
-        int ret=dict[key];
-        list.Remove(key);
-        list.Add(key);
-        return ret;
+        if (_cacheMap.TryGetValue(key, out var node))
+        {
+            _cacheList.Remove(node);
+            _cacheList.AddFirst(node);
+            return node.Value.Value;
+        }
+        return -1;
     }
     
     public void Put(int key, int value) {
-      if(dict.ContainsKey(key))
+        if (_cacheMap.TryGetValue(key, out var node))
         {
-            dict[key]=value;
-            list.Remove(key);
-            list.Add(key);
-            return;
+            node.Value.Value = value;
+            _cacheList.Remove(node);
+            _cacheList.AddFirst(node);
         }
-        if(dict.Count==cap)
+        else
         {
-            int evit=list[0];
-            dict.Remove(evit);
-            list.Remove(evit);
+            if (_cacheMap.Count >= _capacity)
+            {
+                var lastNode = _cacheList.Last;
+                _cacheMap.Remove(lastNode.Value.Key);
+                _cacheList.RemoveLast();
+            }
+
+            var newNode = new LinkedListNode<CacheItem>(new CacheItem(key, value));
+            _cacheMap.Add(key, newNode);
+            _cacheList.AddFirst(newNode);
         }
-        dict.Add(key,value);
-        list.Add(key);
+    }
+
+    private class CacheItem
+    {
+        public int Key { get; }
+        public int Value { get; set; }
+
+        public CacheItem(int key, int value)
+        {
+            Key = key;
+            Value = value;
+        }
     }
 }
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache obj = new LRUCache(capacity);
- * int param_1 = obj.Get(key);
- * obj.Put(key,value);
- */
