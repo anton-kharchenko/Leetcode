4c2c169bb82555c36b28ecf065f9e2dbccb21949
Anton Kharchenko <55918247+anton-kharchenko@users.noreply.github.com>
2024-07-15 20:55:17 +0300

    Time: 718 ms (14.28%), Space: 144.6 MB (9.03%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cs
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cs
@@ -1,26 +1,20 @@
 public class MedianFinder {
-
-    PriorityQueue<int, int> _minHeap = new (Comparer<int>.Create((x,y)=>y.CompareTo(x)));
-    PriorityQueue<int, int> _maxHeap = new ();
+    PriorityQueue<int, int> minHeap = new PriorityQueue<int, int>(Comparer<int>.Create((x, y)=>y.CompareTo(x)));
+    PriorityQueue<int, int> maxHeap = new();
 
     public void AddNum(int num) {
-        _minHeap.Enqueue(num,  num);
-        
-        var maxNumberInMeanQueue = _minHeap.Dequeue(); 
-        _maxHeap.Enqueue(maxNumberInMeanQueue, maxNumberInMeanQueue);
-
-        if (_maxHeap.Count <= _minHeap.Count) return;
-        
-        var minNumberInMaxQueue = _maxHeap.Dequeue();
-        _minHeap.Enqueue(minNumberInMaxQueue, minNumberInMaxQueue);
+        minHeap.Enqueue(num, num);
+        var last = minHeap.Dequeue();
+        maxHeap.Enqueue(last, last);
+        if(minHeap.Count >= maxHeap.Count) return;
+        var minInMax = maxHeap.Dequeue();
+        minHeap.Enqueue(minInMax, minInMax);
     }
     
     public double FindMedian() {
-        if (_minHeap.Count == _maxHeap.Count)
-        {
-            return (_minHeap.Peek() + (double)_maxHeap.Peek())/2;
+        if(minHeap.Count == maxHeap.Count){
+            return ((double)minHeap.Peek() + maxHeap.Peek()) / 2;
         }
-        
-        return _minHeap.Count > _maxHeap.Count ? _minHeap.Peek() : _maxHeap.Peek();
+        return minHeap.Count > maxHeap.Count ? minHeap.Peek() : maxHeap.Peek();
     }
 }
