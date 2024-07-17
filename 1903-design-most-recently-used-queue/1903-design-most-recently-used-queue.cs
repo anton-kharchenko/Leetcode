eb2758261fe820de247b90cddb1c16f13276f002
Anton Kharchenko <55918247+anton-kharchenko@users.noreply.github.com>
2024-07-17 16:18:54 +0300

    Time: 179 ms (85.71%), Space: 89.9 MB (10.71%) - LeetHub

--- 1903-design-most-recently-used-queue/1903-design-most-recently-used-queue.cs
+++ 1903-design-most-recently-used-queue/1903-design-most-recently-used-queue.cs
@@ -1,17 +1,21 @@
 public class MRUQueue {
-    SortedList<int, int> sl = new SortedList<int, int>();
-    int count = 0;
-    public MRUQueue(int n) {
+    private SortedList<int, int> _sortedList;
+    private int count;
+    public MRUQueue(int n)
+    {
+        _sortedList = new SortedList<int, int>();
+        for (int i = 1; i <= n; i++)
+            _sortedList.Add(i, i);
+        
         count = n;
-        for(var i = 1; i<=n; i++)
-            sl.Add(i, i);
     }
     
-    public int Fetch(int k) {
+    public int Fetch(int k)
+    {
         count += 1;
-        var result = sl.Values[k - 1];
-        sl.RemoveAt(k - 1);
-        sl.Add(count, result);
+        var result = _sortedList.Values[k - 1];
+        _sortedList.RemoveAt(k - 1);
+        _sortedList.Add(count, result);
         return result;
     }
 }
