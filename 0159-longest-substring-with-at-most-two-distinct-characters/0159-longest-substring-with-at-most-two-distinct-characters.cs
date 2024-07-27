8bd00e40e2a280f43a8a267a219b0cc20b471057
Anton Kharchenko <55918247+anton-kharchenko@users.noreply.github.com>
2024-07-27 17:37:06 +0300

    Time: 87 ms (47.73%), Space: 44.3 MB (40.91%) - LeetHub

--- 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.cs
+++ 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.cs
@@ -1,33 +1,27 @@
 public class Solution {
     public int LengthOfLongestSubstringTwoDistinct(string s) {
-        int windowStart = 0;
-        int windowEnd = 0;
-        int windowSize = 0;
-        int longestSubstringLength = 0;
-        Dictionary<char, int> dictionary = new Dictionary<char, int>();
-        
-        while(windowEnd<s.Length){ // O(N), O(1)
-            char letter = s[windowEnd];
-            if(dictionary.ContainsKey(letter)){
-                dictionary[letter] += 1;
-            }else{
-                dictionary.Add(letter, 1);
-            }
-            windowEnd += 1;
+        var hs = new Dictionary<char, int>();
+        var start = 0;
+        var end = 0;
+        var result = 0;
+        while (end < s.Length)
+        {
+            hs.TryAdd(s[end], 0);
+            hs[s[end]] += 1;
             
-            if(dictionary.Count > 2){
-                char letterToDelete = s[windowStart];
-                dictionary[letterToDelete] -= 1;
-                if(dictionary[letterToDelete] == 0){
-                    dictionary.Remove(letterToDelete);
-                }
-                windowStart += 1;
+            while (hs.Count > 2)
+            {
+                var letterToDelete = s[start];
+                start += 1;
+                hs[letterToDelete] -= 1;
+                if(hs[letterToDelete] == 0)
+                    hs.Remove(letterToDelete);
             }
             
-            windowSize =  windowEnd - windowStart;
-            longestSubstringLength = Math.Max(longestSubstringLength, windowSize);
+            result = Math.Max(result, end - start + 1);
+            end += 1;
         }
-        
-        return longestSubstringLength;
+
+        return result;
     }
 }
