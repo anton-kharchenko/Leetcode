c5cbf3686370d56b18b58238cbb0c3e769f423dd
Anton Kharchenko <55918247+anton-kharchenko@users.noreply.github.com>
2024-07-27 17:27:12 +0300

    Time: 39 ms (99.43%), Space: 42.6 MB (60.54%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cs
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cs
@@ -1,25 +1,25 @@
 public class Solution {
-    public int LengthOfLongestSubstring(string s) {
+    public int LengthOfLongestSubstring(string s)
+    {
+        var hs = new HashSet<char>();
         var start = 0;
         var end = 0;
-        var hs = new HashSet<char>();
-        var maxWindow = 0;
-        var currentWindow = 0;
-        while(end<s.Length){
+        var result = 0;
+        while (end < s.Length)
+        {
             var letter = s[end];
-            while(hs.Contains(letter)){
+            while (hs.Contains(letter))
+            {
                 var letterToDelete = s[start];
-                hs.Remove(letterToDelete);
                 start += 1;
+                hs.Remove(letterToDelete);
             }
 
             hs.Add(letter);
-            currentWindow = end - start + 1;
-            maxWindow = Math.Max(maxWindow, currentWindow);
+            result = Math.Max(result, hs.Count);
             end += 1;
         }
-        return maxWindow;
+
+        return result;
     }
 }
-
-   
