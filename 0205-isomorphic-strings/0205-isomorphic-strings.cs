46d70da80c0c197337ed4e8e67ccd7ef393e4038
Anton Kharchenko <55918247+anton-kharchenko@users.noreply.github.com>
2024-08-31 19:05:06 +0300

    Time: 67 ms (52.3%), Space: 41.9 MB (81.39%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cs
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cs
@@ -5,7 +5,16 @@ public class Solution
         var map = new Dictionary<char, char>();
         for (int i = 0; i < s.Length; i++)
         {
-            if (map.ContainsKey(s[i]) && map[s[i]] != t[i]) return false;
+            if (map.ContainsKey(s[i]))
+            {
+                if (map[s[i]] != t[i])
+                    return false;
+            }
+            else if (map.ContainsValue(t[i])) 
+            {
+                 return false;
+            }
+
             map.TryAdd(s[i], t[i]);
         }
 
