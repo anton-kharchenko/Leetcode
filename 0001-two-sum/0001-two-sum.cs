a63184612d10c9a293868d95402b9201d26bdf91
Anton Kharchenko <55918247+anton-kharchenko@users.noreply.github.com>
2024-09-23 16:50:38 +0300

    Time: 108 ms (91.68%), Space: 48.2 MB (20.36%) - LeetHub

--- 0001-two-sum/0001-two-sum.cs
+++ 0001-two-sum/0001-two-sum.cs
@@ -1,15 +1,14 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
         var dict = new Dictionary<int, int>();
-        
         for(var i = 0; i<nums.Length; i++){
             if(dict.ContainsKey(target - nums[i])){
-                return new int[2]{i, dict[target - nums[i]]};
-            }else if(!dict.ContainsKey(nums[i])){
-                dict.Add(nums[i], i);
+                return new int[2]{dict[target - nums[i]], i};
+            }else{
+                dict.TryAdd(nums[i], i);
+                dict[nums[i]] = i;
             }
         }
-        
         return new int[2];
-    } 
+    }
 }
