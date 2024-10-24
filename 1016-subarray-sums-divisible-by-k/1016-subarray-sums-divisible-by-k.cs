93a67f2d20b8fa32aa278b54e331f3849498b83a
Anton Kharchenko <55918247+anton-kharchenko@users.noreply.github.com>
2024-10-24 10:24:54 +0300

    Time: 7 ms (98.31%), Space: 51.5 MB (6.78%) - LeetHub

--- 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.cs
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.cs
@@ -2,24 +2,19 @@ public class Solution {
     public int SubarraysDivByK(int[] nums, int k) {
         var count = 0;
         var sum = 0;
-        var map = new Dictionary<int, int>();
-        var length = nums.Length;
-        map.Add(0, 1);
-        for(var i = 0; i<length; i++){
-            sum += nums[i];
+        var map = new Dictionary<int, int> { { 0, 1 } };
+
+        foreach (var number in nums)
+        {
+            sum += number;
             var rest = sum % k;
-            if(rest < 0){
-                rest += k; // TODO
-            }
-
-            if(map.ContainsKey(rest)){
-                count += map[rest];
-                map[rest] += 1;
-            }else{
-                map[rest] = 1;
-            }
+            if(rest < 0)   rest += k; // TODO
+            
+            if (map.TryAdd(rest, 1)) continue;
+            count += map[rest];
+            map[rest] += 1;
         }
-
+        
         return count;
     }
 }
