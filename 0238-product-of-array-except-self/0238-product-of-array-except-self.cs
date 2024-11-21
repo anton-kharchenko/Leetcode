f56600f8c5c8dfd7f63ca00706d0bb7a3e82e68c
Anton Kharchenko <55918247+anton-kharchenko@users.noreply.github.com>
2024-11-21 13:21:10 +0200

    Time: 186 ms (5.23%), Space: 68.1 MB (7.55%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cs
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cs
@@ -1,24 +1,30 @@
-public class Solution {
-    public int[] ProductExceptSelf(int[] nums) {
+public class Solution
+{
+    public int[] ProductExceptSelf(int[] nums)
+    {
         var length = nums.Length;
-        var leftProduct = new int[length];
-        var rightProduct = new int[length];
-        var result = new int[length];
-        Array.Fill(leftProduct, 1);
-        Array.Fill(rightProduct, 1);
 
-        for(var i = 1; i<length; i++){
-            leftProduct[i] = leftProduct[i - 1] * nums[i-1];
+        var left = new int[length];
+        Array.Fill(left, 1);
+        for (int i = 1; i < length; i++)
+        {
+            left[i] = left[i - 1] * nums[i - 1];
         }
 
-        for(var i = length - 2; i>= 0; i--){
-            rightProduct[i] = rightProduct[i + 1] * nums[i + 1];
+        var right = new int[length];
+        Array.Fill(right, 1);
+        for (int i = length - 2; i >= 0; i--)
+        {
+            right[i] = right[i + 1] * nums[i + 1];
         }
 
-        for(var i = 0; i<length; i++){
-            result[i] = leftProduct[i] * rightProduct[i];
+        var product = new int[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            product[i] = left[i] * right[i];
         }
 
-        return result;
+        return product;
     }
 }
